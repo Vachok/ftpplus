598e55127ccc90ad5275f37306ecabf1b79ee515
Vachok <143500@gmail.com>
2018-08-23 01:34:40 +0300

    feat(netscan):

--- src/main/java/ru/vachok/networker/web/ApplicationConfiguration.java
+++ src/main/java/ru/vachok/networker/web/ApplicationConfiguration.java
@@ -3,12 +3,10 @@ package ru.vachok.networker.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@EnableAutoConfiguration
 @ComponentScan
 public class ApplicationConfiguration {
 
--- src/main/java/ru/vachok/networker/web/controller/NetScanner.java
+++ src/main/java/ru/vachok/networker/web/controller/NetScanner.java
@@ -4,8 +4,8 @@ package ru.vachok.networker.web.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import ru.vachok.mysqlandprops.DataConnectTo;
 import ru.vachok.mysqlandprops.RegRuMysql;
 import ru.vachok.networker.web.ApplicationConfiguration;
@@ -17,85 +17,76 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 /**
- @since 21.08.2018 (14:40) */
+ * @since 21.08.2018 (14:40)
+ */
 @Controller
 public class NetScanner {
 
 
-   @GetMapping ("/netscan")
-   @ResponseBody
-   public List<String> getPCNames( HttpServletRequest request ) throws IOException {
-
-      List<String> pcNames = new ArrayList<>();
-      String nameCount;
-      List<String> aList = nextA();
-      InetAddress inetAddress;
-      try{
-         for (int i = 1; i < 200; i++) {
-            nameCount = String.format("%04d", i);
-            inetAddress = InetAddress.getByName("do" + nameCount + ".eatmeat.ru");
-            boolean reachable = inetAddress.isReachable(100);
-            String e = inetAddress.toString() + " online " + reachable;
-            pcNames.add(e);
-            ApplicationConfiguration.logger().warn(e);
-         }
-      } catch (UnknownHostException re) {
-         pcNames.add(re.getMessage());
-         writeDB(pcNames);
-         ApplicationConfiguration.logger().error(re.getMessage() , re);
-      }
-      boolean b = writeDB(pcNames);
-      pcNames.add(b + " WRITE TO DB");
-      boolean addAll = pcNames.addAll(aList);
-      ApplicationConfiguration.logger().warn(addAll + " add all");
-      return pcNames;
-   }
-
-
-   @ResponseBody
-   private List<String> nextA() {
-      String nameCount;
-      List<String> pcNames = new ArrayList<>();
-      InetAddress inetAddress;
-      try {
-         for (int i = 100; i < 350; i++) {
-            nameCount = String.format("%03d" , i);
-            inetAddress = InetAddress.getByName("a" + nameCount + ".eatmeat.ru");
-            boolean reachable = inetAddress.isReachable(100);
-            String e = inetAddress.toString() + " online " + reachable;
-            pcNames.add(e);
-            ApplicationConfiguration.logger().warn(e);
-         }
-      } catch (IOException re) {
-         pcNames.add(re.getMessage());
-         writeDB(pcNames);
-
-         ApplicationConfiguration.logger().error(re.getMessage() , re);
-      }
-      return pcNames;
-   }
-
-
-   private boolean writeDB( List<String> pcNames ) {
-       DataConnectTo dataConnectTo = new RegRuMysql();
-
-       try (Connection c = dataConnectTo.getDataSource().getConnection(); PreparedStatement p = c.prepareStatement("insert into  u0466446_liferpg.velkompc (instr) values (?)")) {
-         pcNames.stream().sorted().forEach(x -> {
+    @GetMapping("/netscan")
+    public String getPCNames( HttpServletRequest request , Model model ) throws IOException {
+        List<String> pcNames = new ArrayList<>();
+        String nameCount;
+        InetAddress inetAddress;
+        String qer = request.getQueryString();
+        String namePref = "do";
+        if (qer != null) {
+            ApplicationConfiguration.logger().info(qer);
+            namePref = qer;
+        }
+        for (int i = 1; i < 350; i++) {
+            nameCount = String.format("%04d" , i);
+            if (namePref.equalsIgnoreCase("a")) nameCount = String.format("%03d" , i);
+            if (namePref.equalsIgnoreCase("td")) nameCount = String.format("%03d" , i);
             try {
-               p.setString(1 , x);
-               p.executeUpdate();
-            } catch (SQLException e) {
-               ApplicationConfiguration.logger().error(e.getMessage() , e);
+                inetAddress = InetAddress.getByName(namePref + nameCount + ".eatmeat.ru");
+            } catch (UnknownHostException e) {
+                continue;
             }
-         });
-          return true;
-      } catch (SQLException e) {
-         ApplicationConfiguration.logger().error(e.getMessage() , e);
-          return false;
-      }
-   }
+            boolean reachable = inetAddress.isReachable(100);
+            String e = inetAddress.toString();
+            if (!reachable) {
+                String onLines = ("<b> online </b><i>" + false + "</i>");
+                pcNames.add(e + "<b>" + onLines + "</b>");
+                ApplicationConfiguration.logger().warn(e + " " + onLines);
+            } else {
+                String onLines = ("<b> online </b>" + true);
+                pcNames.add(e + "<b>" + onLines + "</b>");
+                ApplicationConfiguration.logger().warn(e + " " + onLines);
+            }
+        }
+        boolean b = writeDB(pcNames);
+        pcNames.add(b + " WRITE TO DB");
+        model.addAttribute("pc" , "<p>" + Arrays.toString(pcNames.toArray()).replaceAll(", " , "<br>") + "</p>");
+        return "netscan";
+    }
+
+
+    private boolean writeDB( List<String> pcNames ) {
+        DataConnectTo dataConnectTo = new RegRuMysql();
+
+        try (Connection c = dataConnectTo.getDataSource().getConnection(); PreparedStatement p = c.prepareStatement("insert into  u0466446_liferpg.velkompc (NamePP, AddressPP, OnlineNow) values(?,?,?)")) {
+            pcNames.stream().sorted().forEach(x -> {
+                boolean onLine = false;
+                try {
+                    if (x.contains("true")) onLine = true;
+                    p.setString(1 , x.split("/")[0]);
+                    p.setString(2 , x.split("/")[1].split(" ")[0].replaceAll("<b>" , ""));
+                    p.setBoolean(3 , onLine);
+                    p.executeUpdate();
+                } catch (SQLException e) {
+                    ApplicationConfiguration.logger().error(e.getMessage() , e);
+                }
+            });
+            return true;
+        } catch (SQLException e) {
+            ApplicationConfiguration.logger().error(e.getMessage() , e);
+            return false;
+        }
+    }
 }
