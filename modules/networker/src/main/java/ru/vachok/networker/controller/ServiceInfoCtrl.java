00729a9d688dacf9a8d0bd3d310da295c6f0d897
Vachok <143500@gmail.com>
2018-09-22 10:32:49 +0300

    feat(serv):

--- modules/networker/src/main/java/ru/vachok/networker/controller/ServiceInfoCtrl.java
+++ modules/networker/src/main/java/ru/vachok/networker/controller/ServiceInfoCtrl.java
@@ -12,6 +12,8 @@ import ru.vachok.networker.services.VisitorSrv;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.file.AccessDeniedException;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * @since 21.09.2018 (11:33)
@@ -29,13 +31,22 @@ public class ServiceInfoCtrl {
             model.addAttribute("title", "Closed section");
             model.addAttribute("urls", new TForms().fromArray(AppCtx.getClassLoaderURLList()));
             model.addAttribute("request", prepareRequest(request));
-            model.addAttribute("genstamp", "Generated: " + new Date().getTime());
+            model.addAttribute("genstamp", "Generated: " +
+                new Date().getTime() +
+                ". Up: " +
+                TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - ConstantsFor.START_STAMP));
             return "vir";
         } else throw new AccessDeniedException("Sorry. Denied");
     }
 
     private String prepareRequest(HttpServletRequest request) {
-
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<center><h3>Заголовки</h3></center>");
+        stringBuilder.append(new TForms().fromEnum(request.getHeaderNames(), true));
+        stringBuilder.append("<center><h3>Атрибуты</h3></center>");
+        stringBuilder.append(new TForms().fromEnum(request.getAttributeNames(), true));
+        stringBuilder.append("<center><h3>Параметры</h3></center>");
+        stringBuilder.append(new TForms().fromEnum(request.getParameterNames(), true));
+        return stringBuilder.toString();
     }
 }
