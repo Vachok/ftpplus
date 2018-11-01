713533a8bdf4aee3274a968247e8c1bcd2d45169
Vachok <143500@gmail.com>
2018-11-01 23:07:41 +0300

    build(231):

--- modules/money/src/main/java/ru/vachok/money/calc/ChooseYouDestiny.java
+++ modules/money/src/main/java/ru/vachok/money/calc/ChooseYouDestiny.java
@@ -1,7 +1,12 @@
 package ru.vachok.money.calc;
 
 
+import ru.vachok.money.ConstantsFor;
+import ru.vachok.mysqlandprops.props.DBRegProperties;
+import ru.vachok.mysqlandprops.props.InitProperties;
+
 import java.security.SecureRandom;
+import java.util.Properties;
 
 
 /**
@@ -16,7 +21,15 @@ public class ChooseYouDestiny {
     private static final String SOURCE_CLASS = ChooseYouDestiny.class.getSimpleName();
 
     String destinyCooser(String[] strings) {
+        InitProperties initProperties = new DBRegProperties(ConstantsFor.APP_NAME + SOURCE_CLASS);
+        Properties properties = initProperties.getProps();
         int secureRandom = new SecureRandom().nextInt(strings.length);
+        for(int i = 0; i < strings.length; i++){
+            if(strings[i].toLowerCase().contains(", ")){
+                strings[i] = String.valueOf(properties.getOrDefault("ans", "Тогда выбери: <br>"));
+            }
+            return strings[i] + strings[secureRandom];
+        }
         return strings[secureRandom];
     }
 }
