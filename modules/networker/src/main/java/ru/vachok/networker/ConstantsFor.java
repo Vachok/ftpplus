901f4e21db94ec179f09380bd673cf29b8400290
Vachok <33656095+Vachok@users.noreply.github.com>
2018-11-07 13:52:54 +0300

    Update modules/networker/src/main/java/ru/vachok/networker/ConstantsFor.java

--- modules/networker/src/main/java/ru/vachok/networker/ConstantsFor.java
+++ modules/networker/src/main/java/ru/vachok/networker/ConstantsFor.java
@@ -47,7 +47,6 @@ public enum ConstantsFor {
     public static final Float NO_F_DAYS = TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() -
         Long.parseLong(getTheProps().getProperty("lasts", 1515233487000L + ""))) / 60f / 24f;
 
-    public static final Map<Long, HttpServletRequest> VISITS_MAP = new ConcurrentHashMap<>();
 
 
     public static final String FOOTER = "footer";
