4b5930d14089786b83715100766be1cedc85f05d
Vachok <143500@gmail.com>
2018-12-04 20:38:08 +0300

    feat(TimeCheck):

--- modules/money/src/main/java/ru/vachok/money/ctrls/SysInfoCtrl.java
+++ modules/money/src/main/java/ru/vachok/money/ctrls/SysInfoCtrl.java
@@ -1,21 +1,27 @@
 package ru.vachok.money.ctrls;
 
 
+import org.apache.commons.net.ntp.TimeInfo;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.vachok.messenger.MessageToUser;
 import ru.vachok.money.components.AppVersion;
 import ru.vachok.money.components.PageFooter;
+import ru.vachok.money.config.AppComponents;
 import ru.vachok.money.config.ThreadConfig;
 import ru.vachok.money.filesys.FilesCleaner;
 import ru.vachok.money.services.DBMessage;
+import ru.vachok.money.services.TimeChecker;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -29,6 +35,9 @@ import java.util.concurrent.Future;
 public class SysInfoCtrl {
 
     private ThreadConfig t = new ThreadConfig();
+
+    /*Fields*/
+    private static final Logger LOGGER = AppComponents.getLogger();
 
     @GetMapping ("/sysinfo")
     public String gettingInfo(Model model, HttpServletRequest request) {
@@ -50,14 +59,29 @@ public class SysInfoCtrl {
         int firstDayOfWeek = Calendar.getInstance().getFirstDayOfWeek();
         Calendar.Builder builder = new Calendar.Builder();
         int toSatDays = satDayOfWeek.getValue() - firstDayOfWeek;
-        Date date = builder.setDate(nowDate.getYear(), nowDate.getMonthValue() - 1, nowDate.getDayOfMonth() + toSatDays).build().getTime();
+        Date date = builder.setDate(
+            nowDate.getYear(),
+            nowDate.getMonthValue() - 1,
+            nowDate.getDayOfMonth() + toSatDays).build().getTime();
+        Callable<TimeInfo> atomCall = new TimeChecker();
+        TimeInfo atomInfo = null;
+        try{
+
+            atomInfo = atomCall.call();
+        }
+        catch(Exception e){
+            LOGGER.warn(e.getMessage());
+        }
         stringBuilder
             .append("<br>До ")
             .append(date.toString())
             .append("<br><font color=\"yellow\">")
             .append(toSatDays)
-            .append("</font> дней...<br>");
-
+            .append("</font> дней...<br>Точное UNIX-время: <font color=\"orange\">")
+            .append(Objects.requireNonNull(atomInfo).getReturnTime())
+            .append("</font><br>Human readable: ")
+            .append(Objects.requireNonNull(atomInfo).getMessage().toString().replaceAll("\\Q[\\E", "")
+                .replaceAll("\\Q]\\E", "").replaceAll(",", "<br>").replaceAll("\\Q:\\E", ": "));
         return stringBuilder.toString();
     }
 
---
+++ modules/money/src/main/java/ru/vachok/money/services/TimeChecker.java
@@ -0,0 +1,42 @@
+package ru.vachok.money.services;
+
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+import org.slf4j.Logger;
+import ru.vachok.money.config.AppComponents;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.Callable;
+
+
+/**
+ @since 04.12.2018 (20:21) */
+public class TimeChecker implements Callable<TimeInfo> {
+
+    /*Fields*/
+    private static final Logger LOGGER = AppComponents.getLogger();
+
+
+    @Override
+    public TimeInfo call() throws Exception {
+        return ntpCheck();
+    }
+
+    /**
+     Получаем время от time.windows.com
+     <p>
+
+     @return {@link TimeInfo}
+     @throws IOException {@link InetAddress} unreachable.
+     */
+    private TimeInfo ntpCheck() throws IOException {
+        NTPUDPClient ntpudpClient = new NTPUDPClient();
+        TimeInfo atomTime = ntpudpClient.getTime(InetAddress.getByName("time.windows.com"));
+        atomTime.computeDetails();
+        String msg = atomTime.getMessage().toString();
+        LOGGER.info(msg);
+        return atomTime;
+    }
+}
