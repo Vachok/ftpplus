27feabc31fb7111c515608c1466856e979ba4635
Vachok <143500@gmail.com>
2018-12-10 17:19:37 +0300

    feat(allowdomain): in progress

--- modules/networker/src/main/java/ru/vachok/networker/accesscontrol/SshActs.java
+++ modules/networker/src/main/java/ru/vachok/networker/accesscontrol/SshActs.java
@@ -10,11 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.vachok.networker.ConstantsFor;
+import ru.vachok.networker.SSHFactory;
 import ru.vachok.networker.componentsrepo.AppComponents;
 import ru.vachok.networker.componentsrepo.PageFooter;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.AccessDeniedException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Objects;
 
 /**
@@ -153,25 +159,6 @@ public class SshActs {
         this.inet = queryString;
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("SshActs{");
-        sb.append("allowDomain='").append(allowDomain).append('\'');
-        sb.append(", AT_NAME_SSHACTS='").append(AT_NAME_SSHACTS).append('\'');
-        sb.append(", AT_NAME_SSHDETAIL='").append(AT_NAME_SSHDETAIL).append('\'');
-        sb.append(", comment='").append(comment).append('\'');
-        sb.append(", inet='").append(inet).append('\'');
-        sb.append(", ipAddrOnly='").append(ipAddrOnly).append('\'');
-        sb.append(", PAGE_NAME='").append(PAGE_NAME).append('\'');
-        sb.append(", pcName='").append(pcName).append('\'');
-        sb.append(", squid=").append(squid);
-        sb.append(", squidLimited=").append(squidLimited);
-        sb.append(", tempFull=").append(tempFull);
-        sb.append(", vipNet=").append(vipNet);
-        sb.append('}');
-        return sb.toString();
-    }
-
     private String execByWhatListSwitcher(int whatList, boolean iDel) {
         if (iDel) {
             return "sudo grep -v '" + Objects.requireNonNull(pcName) + "' /etc/pf/vipnet > /etc/pf/vipnet_tmp;sudo grep -v '" + Objects.requireNonNull(ipAddrOnly) + "' /etc/pf/vipnet > /etc/pf/vipnet_tmp;sudo cp /etc/pf/vipnet_tmp /etc/pf/vipnet;sudo pfctl -f /etc/srv-nat.conf;sudo grep -v '" + Objects.requireNonNull(ipAddrOnly) + "' /etc/pf/squid > /etc/pf/squid_tmp;sudo cp /etc/pf/squid_tmp /etc/pf/squid;sudo grep -v '" + Objects.requireNonNull(ipAddrOnly) + "' /etc/pf/squidlimited > /etc/pf/squidlimited_tmp;sudo cp /etc/pf/squidlimited_tmp /etc/pf/squidlimited;sudo grep -v '" + Objects.requireNonNull(ipAddrOnly) + "' /etc/pf/tempfull > /etc/pf/tempfull_tmp;sudo cp /etc/pf/tempfull_tmp /etc/pf/tempfull;sudo squid -k reconfigure;sudo /etc/initpf.fw";
@@ -194,6 +181,39 @@ public class SshActs {
     }
 
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("SshActs{");
+        sb.append("allowDomain='").append(allowDomain).append('\'');
+        sb.append(", AT_NAME_SSHACTS='").append(AT_NAME_SSHACTS).append('\'');
+        sb.append(", AT_NAME_SSHDETAIL='").append(AT_NAME_SSHDETAIL).append('\'');
+        sb.append(", comment='").append(comment).append('\'');
+        sb.append(", inet='").append(inet).append('\'');
+        sb.append(", ipAddrOnly='").append(ipAddrOnly).append('\'');
+        sb.append(", PAGE_NAME='").append(PAGE_NAME).append('\'');
+        sb.append(", pcName='").append(pcName).append('\'');
+        sb.append(", squid=").append(squid);
+        sb.append(", squidLimited=").append(squidLimited);
+        sb.append(", tempFull=").append(tempFull);
+        sb.append(", vipNet=").append(vipNet);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    private void allowDomainAct() {
+        String ipResolved = null;
+        try {
+            ipResolved = InetAddress.getByName(allowDomain).toString();
+        } catch (UnknownHostException e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+        String commandSSH = "sudo echo " + "\"" + allowDomain + "\"" + " >> /etc/pf/allowdomain;sudo echo " + "\"" + ipResolved + comment + "\"" + " >> /etc/pf/allowip;sudo /etc/initpf.fw;sudo squid -k reconfigure;sudo /etc/initpf.fw;sudo tail /etc/pf/allowdomain;exit";
+        commandSSH = "ls";
+        LOGGER.warn(new SSHFactory.Builder(ConstantsFor.SRV_NAT, commandSSH).build().call());
+
+    }
+
+
     /**
      {@link Controller}, для работы с SSH
 
@@ -202,18 +222,22 @@ public class SshActs {
     @Controller
     public class SshActsCTRL {
 
+        /**
+         {@link SshActs}
+         */
+        @Autowired
+        private SshActs sshActs;
+
         @PostMapping("/sshacts")
         public String sshActsPOST(@ModelAttribute SshActs sshActs, Model model, HttpServletRequest request) throws AccessDeniedException {
             String pcReq = request.getRemoteAddr().toLowerCase();
-            if(getAuthentic(pcReq)){
+            if (getAuthentic(pcReq)) {
                 this.sshActs = sshActs;
                 model.addAttribute(AT_NAME_SSHACTS, sshActs);
                 model.addAttribute(AT_NAME_SSHDETAIL, sshActs.getPcName());
                 return PAGE_NAME;
             } else throw new AccessDeniedException("NOT Allowed!");
         }
-
-        private SshActs sshActs;
 
         @Autowired
         public SshActsCTRL(SshActs sshActs) {
@@ -233,8 +257,8 @@ public class SshActs {
             String pcReq = request.getRemoteAddr().toLowerCase();
             LOGGER.warn(pcReq);
             setInet(pcReq);
-            if(getAuthentic(pcReq)){
-                model.addAttribute(ConstantsFor.TITLE, "SSH Works");
+            if (getAuthentic(pcReq)) {
+                model.addAttribute(ConstantsFor.TITLE, percToEnd());
                 model.addAttribute(ConstantsFor.FOOTER, new PageFooter().getFooterUtext());
                 model.addAttribute(AT_NAME_SSHACTS, sshActs);
                 if (request.getQueryString() != null) {
@@ -248,13 +272,21 @@ public class SshActs {
             } else throw new AccessDeniedException("NOT Allowed! ");
         }
 
+        private String percToEnd() {
+            LocalTime endDay = LocalTime.parse("17:30");
+            LocalDateTime localDateTime = endDay.atDate(LocalDate.now());
+            return localDateTime.toString();
+        }
+
         @PostMapping("/allowdomain")
         public String allowPOST(@ModelAttribute SshActs sshActs, Model model) {
             this.sshActs = sshActs;
+            allowDomainAct();
             model.addAttribute(ConstantsFor.TITLE, sshActs.getAllowDomain());
             model.addAttribute(AT_NAME_SSHACTS, sshActs);
-            model.addAttribute(AT_NAME_SSHDETAIL, sshActs.toString());
-            return "redirect:/sshacts";
+            model.addAttribute("ok", sshActs.toString());
+            model.addAttribute(ConstantsFor.FOOTER, new PageFooter().getFooterUtext());
+            return "ok";
         }
     }
 }
