f557cdd4f2f94b59a33adb99ac852cf41fb694a3
Vachok <143500@gmail.com>
2018-12-27 22:40:42 +0300

    fix:
    17 minutes

--- modules/networker/src/main/java/ru/vachok/networker/net/DiapazonedScan.java
+++ modules/networker/src/main/java/ru/vachok/networker/net/DiapazonedScan.java
@@ -18,7 +18,6 @@ import java.net.InetAddress;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -29,7 +28,7 @@ import static ru.vachok.networker.componentsrepo.AppComponents.getLogger;
  Скан диапазона адресов
 
  @since 19.12.2018 (11:35) */
-@SuppressWarnings({"FieldNotUsedInToString", "DoubleCheckedLocking"})
+@SuppressWarnings ({"FieldNotUsedInToString", "DoubleCheckedLocking"})
 public class DiapazonedScan implements Runnable {
 
     /**
@@ -48,6 +47,11 @@ public class DiapazonedScan implements Runnable {
     private static final String ROOT_PATH_STR = Paths.get(".").toAbsolutePath().toString();
 
     /**
+     {@link NetScanFileWorker#getI()}
+     */
+    private static final NetScanFileWorker NET_SCAN_FILE_WORKER = NetScanFileWorker.getI();
+
+    /**
      Singleton inst
      */
     private static volatile DiapazonedScan ourInstance = null;
@@ -61,30 +65,19 @@ public class DiapazonedScan implements Runnable {
     private String fileTimes = "";
 
     /**
-     {@link NetScanFileWorker#getI()}
-     */
-    private static final NetScanFileWorker NET_SCAN_FILE_WORKER = NetScanFileWorker.getI();
-
-    /**
      SINGLETON
 
      @return single.
      */
     public static DiapazonedScan getInstance() {
-        if (ourInstance == null) {
-            synchronized (DiapazonedScan.class) {
-                if (ourInstance == null) {
+        if(ourInstance==null){
+            synchronized(DiapazonedScan.class) {
+                if(ourInstance==null){
                     ourInstance = new DiapazonedScan();
                 }
             }
         }
         return ourInstance;
-    }
-
-    /**
-     Singleton
-     */
-    private DiapazonedScan() {
     }
 
     /**
@@ -95,31 +88,9 @@ public class DiapazonedScan implements Runnable {
     }
 
     /**
-     {@link #scanNew()}
+     Singleton
      */
-    @Override
-    public void run() {
-        LOGGER.warn("DiapazonedScan.run");
-        scanNew();
-    }
-
-    /**
-     @return /showalldev = {@link NetScanCtr#allDevices(Model, HttpServletRequest, HttpServletResponse)}
-     */
-    @Override
-    public String toString() {
-        LOGGER.info("DiapazonedScan.toString");
-        final StringBuilder sb = new StringBuilder("DiapazonedScan{ ");
-        sb
-            .append("<a href=\"/showalldev\">ALL_DEVICES ")
-            .append(ConstantsFor.ALL_DEVICES.size())
-            .append("/5610 (")
-            .append((float) ConstantsFor.ALL_DEVICES.size() / (float) (ConstantsFor.IPS_IN_VELKOM_VLAN / 100))
-            .append(" %)");
-        sb.append("</a>}");
-        sb.append(" ROOT_PATH_STR= ").append(ROOT_PATH_STR);
-        sb.append("<br>NetScanFileWorker hash= ").append(NET_SCAN_FILE_WORKER.hashCode());
-        return sb.toString();
+    private DiapazonedScan() {
     }
 
     /**
@@ -135,7 +106,7 @@ public class DiapazonedScan implements Runnable {
         StringBuilder stringBuilder = new StringBuilder();
         Field[] swFields = Switches.class.getFields();
         List<String> swList = new ArrayList<>();
-        for (Field swF : swFields) {
+        for(Field swF : swFields){
             swList.add("\n" + swF.get(swF).toString());
         }
         swList.forEach(stringBuilder::append);
@@ -145,25 +116,39 @@ public class DiapazonedScan implements Runnable {
     }
 
     /**
+     {@link #scanNew()}
+     */
+    @Override
+    public void run() {
+        LOGGER.warn("DiapazonedScan.run");
+
+        scanNew();
+    }
+
+    /**
      Добавляет в {@link ConstantsFor#ALL_DEVICES} адреса <i>10.200.200-217.254</i>
      */
-    @SuppressWarnings("all")
+    @SuppressWarnings ("all")
     private void scanNew() {
         final long stArt = System.currentTimeMillis();
         Path p = Paths.get(ROOT_PATH_STR + "\\lan\\200_" + System.currentTimeMillis() / 1000 + ".scan");
-        this.fileTimes = this.fileTimes + "\n" + p.toString() + " " + new Date(p.toFile().lastModified()).toString() + "\n";
+
         String msg1 = "DiapazonedScan.scanNew " + p.toAbsolutePath().toString();
         LOGGER.warn(msg1);
         File newLanFile = new File(ConstantsFor.AVAILABLE_LAST_TXT);
-        try (OutputStream outputStream = new FileOutputStream(newLanFile);
-             PrintWriter printWriter = new PrintWriter(outputStream, true)) {
-            if (ConstantsFor.ALL_DEVICES.remainingCapacity() == 0) {
+        try(OutputStream outputStream = new FileOutputStream(newLanFile);
+            PrintWriter printWriter = new PrintWriter(outputStream, true)){
+            if(ConstantsFor.ALL_DEVICES.remainingCapacity()==0){
                 MessageToUser messageToUser = new DBMessenger();
                 messageToUser.infoNoTitles(new TForms().fromArray(ConstantsFor.ALL_DEVICES));
                 ConstantsFor.ALL_DEVICES.clear();
                 scanLan(printWriter, 200, 218, stArt, "10.200.");
-            } else scanLan(printWriter, 200, 218, stArt, "10.200.");
-        } catch (IOException e) {
+            }
+            else{
+                scanLan(printWriter, 200, 218, stArt, "10.200.");
+            }
+        }
+        catch(IOException e){
             LOGGER.error(e.getMessage(), e);
             String msg = "Vlans 200-217 completed.\nTime spend: " +
                 TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - stArt) + "\n\n";
@@ -172,6 +157,50 @@ public class DiapazonedScan implements Runnable {
         boolean b = FileSystemWorker.copyOrDelFile(newLanFile, p.toAbsolutePath().toString(), false);
         NET_SCAN_FILE_WORKER.setNewLanLastScan(p.toFile());
         scanOldLan(stArt);
+    }
+
+    /**
+     Сканер локальной сети
+
+     @param printWriter Запись в лог
+     @param fromVlan    начало с 3 октета IP
+     @param toVlan      конец с 3 октета IP
+     @param stArt       таймер
+     @param whatVlan    первый 2 октета, с точкоё в конце.
+     @throws IOException запись в файл
+     */
+    @SuppressWarnings ("MethodWithMultipleLoops")
+    private void scanLan(PrintWriter printWriter, int fromVlan, int toVlan, long stArt, String whatVlan) throws IOException {
+        for(int i = fromVlan; i < toVlan; i++){
+            StringBuilder msgBuild = new StringBuilder();
+            for(int j = 0; j < 255; j++){
+                msgBuild = new StringBuilder();
+                byte[] aBytes = InetAddress.getByName(whatVlan + i + "." + j).getAddress();
+                InetAddress byAddress = InetAddress.getByAddress(aBytes);
+                int t = 100;
+                if(ConstantsFor.thisPC().toLowerCase().contains("home")){
+                    t = 400;
+                }
+                if(byAddress.isReachable(t)){
+                    printWriter.println(byAddress.getHostName() + " " + byAddress.getHostAddress());
+                    ConstantsFor.ALL_DEVICES.add("<font color=\"green\">" + byAddress.toString() + FONT_BR_STR);
+                }
+                else{
+                    ConstantsFor.ALL_DEVICES.add("<font color=\"red\">" + byAddress.toString() + FONT_BR_STR);
+                }
+                msgBuild.append("IP was ").append(whatVlan).append(i).append("<-i.j->").append(j).append("\n")
+                    .append(j).append(" was j\n");
+                String msg = msgBuild.toString();
+                LOGGER.info(msg);
+            }
+            msgBuild
+                .append(i).append(" was i. Total time: ")
+                .append(TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - stArt))
+                .append("min\n").append(ConstantsFor.ALL_DEVICES.size()).append(" ALL_DEVICES.size()");
+            String msg = msgBuild.toString();
+            LOGGER.warn(msg);
+            printWriter.println(msg);
+        }
     }
 
     /**
@@ -188,21 +217,15 @@ public class DiapazonedScan implements Runnable {
             .append("\\lan\\192_")
             .append(System.currentTimeMillis() / 1000)
             .append(".scan").toString());
-        this.fileTimes = new StringBuilder()
-            .append(this.fileTimes)
-            .append("\n")
-            .append(p.toString())
-            .append(" ")
-            .append(new Date(p.toFile().lastModified()).toString())
-            .append("\n").toString();
         String msg1 = new StringBuilder()
             .append("scanOldLan ")
             .append(p.toAbsolutePath().toString()).toString();
         LOGGER.warn(msg1);
-        try (OutputStream outputStream = new FileOutputStream(oldLANFile);
-             PrintWriter printWriter = new PrintWriter(outputStream, true)) {
+        try(OutputStream outputStream = new FileOutputStream(oldLANFile);
+            PrintWriter printWriter = new PrintWriter(outputStream, true)){
             scanLan(printWriter, 11, 15, stArt, "192.168.");
-        } catch (IOException e) {
+        }
+        catch(IOException e){
             LOGGER.error(e.getMessage());
         }
         boolean b = FileSystemWorker.copyOrDelFile(oldLANFile, p.toAbsolutePath().toString(), false);
@@ -213,46 +236,34 @@ public class DiapazonedScan implements Runnable {
     }
 
     /**
-     Сканер локальной сети
-
-     @param printWriter Запись в лог
-     @param fromVlan    начало с 3 октета IP
-     @param toVlan      конец с 3 октета IP
-     @param stArt       таймер
-     @param whatVlan    первый 2 октета, с точкоё в конце.
-     @throws IOException запись в файл
+     @return /showalldev = {@link NetScanCtr#allDevices(Model, HttpServletRequest, HttpServletResponse)}
      */
-    @SuppressWarnings("MethodWithMultipleLoops")
-    private void scanLan(PrintWriter printWriter, int fromVlan, int toVlan, long stArt, String whatVlan) throws IOException {
-        for (int i = fromVlan; i < toVlan; i++) {
-            StringBuilder msgBuild = new StringBuilder();
-            for (int j = 0; j < 255; j++) {
-                msgBuild = new StringBuilder();
-                byte[] aBytes = InetAddress.getByName(whatVlan + i + "." + j).getAddress();
-                InetAddress byAddress = InetAddress.getByAddress(aBytes);
-                int t = 100;
-                if (ConstantsFor.thisPC().toLowerCase().contains("home")) {
-                    t = 400;
-                }
-                if (byAddress.isReachable(t)) {
-                    printWriter.println(byAddress.getHostName() + " " + byAddress.getHostAddress());
-                    ConstantsFor.ALL_DEVICES.add("<font color=\"green\">" + byAddress.toString() + FONT_BR_STR);
-                } else {
-                    ConstantsFor.ALL_DEVICES.add("<font color=\"red\">" + byAddress.toString() + FONT_BR_STR);
-                }
-                msgBuild.append("IP was ").append(whatVlan).append(i).append("<-i.j->").append(j).append("\n")
-                    .append(j).append(" was j\n");
-                String msg = msgBuild.toString();
-                LOGGER.info(msg);
-            }
-            msgBuild
-                .append(i).append(" was i. Total time: ")
-                .append(TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - stArt))
-                .append("min\n").append(ConstantsFor.ALL_DEVICES.size()).append(" ALL_DEVICES.size()");
-            String msg = msgBuild.toString();
-            LOGGER.warn(msg);
-            printWriter.println(msg);
+    @Override
+    public String toString() {
+        try{
+            fileTimes = ConstantsFor.AVAILABLE_LAST_TXT + " " +
+                Paths.get(ConstantsFor.AVAILABLE_LAST_TXT).toFile().lastModified() + "\n" +
+                ConstantsFor.OLD_LAN_TXT +
+                " " +
+                Paths.get(ConstantsFor.OLD_LAN_TXT).toFile().lastModified();
         }
+        catch(NullPointerException e){
+            LOGGER.info("NO FILES!");
+        }
+        LOGGER.info("DiapazonedScan.toString");
+        final StringBuilder sb = new StringBuilder("DiapazonedScan{ ");
+        sb
+            .append("<a href=\"/showalldev\">ALL_DEVICES ")
+            .append(ConstantsFor.ALL_DEVICES.size())
+            .append("/5610 (")
+            .append(( float ) ConstantsFor.ALL_DEVICES.size() / ( float ) (ConstantsFor.IPS_IN_VELKOM_VLAN / 100))
+            .append(" %)");
+        sb.append("</a>}");
+        sb.append(" ROOT_PATH_STR= ").append(ROOT_PATH_STR);
+        sb.append(" fileTimes= ").append(fileTimes);
+        sb.append("<br>NetScanFileWorker hash= ").append(NET_SCAN_FILE_WORKER.hashCode());
+
+        return sb.toString();
     }
 
 }
