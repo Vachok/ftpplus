1fc0a039aab4aa2bcbbaa38be71867cd1d22a975
Vachok <143500@gmail.com>
2019-01-11 20:40:32 +0300

    fix:
    50 seconds

--- modules/networker/src/main/java/ru/vachok/networker/controller/ServiceInfoCtrl.java
+++ modules/networker/src/main/java/ru/vachok/networker/controller/ServiceInfoCtrl.java
@@ -185,7 +185,7 @@ public class ServiceInfoCtrl {
         stringStringConcurrentMap.forEach((x, y) -> {
             try{
                 retListStr.add("<b>" + x.split("FtpClientPlus")[1] + "</b>");
-                retListStr.add(y.split("session=")[0]);
+                retListStr.add(y.split("идёт сессия")[0]);
             }
             catch(Exception e){
                 retListStr.add(e.getMessage());
