5f307627460c4cafb8f2b36bdb4478adc02b5625
Vachok <143500@gmail.com>
2019-04-14 01:59:14 +0300

    ftpplus-74 Inetstats

--- modules/networker/src/main/java/ru/vachok/networker/services/WeekPCStats.java
+++ modules/networker/src/main/java/ru/vachok/networker/services/WeekPCStats.java
@@ -3,6 +3,7 @@ package ru.vachok.networker.services;
 
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import ru.vachok.messenger.MessageSwing;
 import ru.vachok.messenger.MessageToUser;
 import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.ConstantsFor;
@@ -12,6 +13,7 @@ import ru.vachok.networker.systray.MessageToTray;
 
 import java.io.*;
 import java.sql.*;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,7 @@ import java.util.List;
 /**
  Сбор статы по-недельно
  <p>
-
+ Устойчивость (in/(in+out)): 2/(2+6) = 0.25 (устойчив на 75%);
  @since 08.12.2018 (0:12) */
 public class WeekPCStats implements Runnable, DataBaseRegSQL {
 
@@ -27,6 +29,9 @@ public class WeekPCStats implements Runnable, DataBaseRegSQL {
      Лист только с именами ПК
      */
     private static final List<String> PC_NAMES_IN_TABLE = new ArrayList<>();
+
+    private static final String FILENAME_INETSTATSIPCSV = "inetstatsIP.csv";
+
     private static final String FILENAME_INETSTATSCSV = "inetstats.csv";
 
     private static MessageToUser messageToUser;
@@ -35,6 +40,8 @@ public class WeekPCStats implements Runnable, DataBaseRegSQL {
 
     private String fileName;
     private IllegalStateException illegalStateException = new IllegalStateException("13.04.2019 (18:17)");
+
+    private static final String SQL_DISTINCTIPSWITHINET = "SELECT DISTINCT `ip` FROM `inetstats`";
 
 
     public WeekPCStats(String sql , String fileName) {
@@ -60,7 +67,8 @@ public class WeekPCStats implements Runnable, DataBaseRegSQL {
         Thread.currentThread().setName("WeekPCStats.run");
         final long stArt = System.currentTimeMillis();
         pcUsrAutoMake();
-        messageToUser.info(getClass().getSimpleName() + "in kbytes. " , new File(FILENAME_INETSTATSCSV).getAbsolutePath() , " = " + cleanInetStats());
+        messageToUser.info(getClass().getSimpleName() + "in kbytes. " , new File(FILENAME_INETSTATSIPCSV).getAbsolutePath() , " = " + readInetStats());
+        readInetStatsRSetToCSV();
     }
 
 
@@ -68,26 +76,40 @@ public class WeekPCStats implements Runnable, DataBaseRegSQL {
         final long stArt = System.currentTimeMillis();
         int retInt = 0;
         File file = new File(fileName);
-        try (Connection c = new AppComponents().connection(ConstantsFor.DBBASENAME_U0466446_VELKOM);
-             PreparedStatement p = c.prepareStatement(sql);
-             ResultSet r = p.executeQuery();
-             OutputStream outputStream = new FileOutputStream(file);
-             PrintWriter printWriter = new PrintWriter(outputStream, true)){
-            while(r.next()){
-                if(sql.equals(ConstantsFor.SQL_SELECTFROM_PCUSERAUTO)) pcUserAutoSelect(r , printWriter);
-                else { cleanInetStatsRSet(r , printWriter); }
+        try(Connection c = new AppComponents().connection(ConstantsFor.DBBASENAME_U0466446_VELKOM)){
+            try(PreparedStatement p = c.prepareStatement(sql)){
+                if(sql.contains("DELETE")) {
+                    int delUp = p.executeUpdate();
+                    new MessageLocal(getClass().getSimpleName()).info(fileName , "deleted" , delUp + " rows.");
+                    return delUp;
+                }
+                try(ResultSet r = p.executeQuery()){
+                    try(OutputStream outputStream = new FileOutputStream(file)){
+                        try(PrintWriter printWriter = new PrintWriter(outputStream , true)){
+                            while(r.next()){
+                                if(sql.equals(ConstantsFor.SQL_SELECTFROM_PCUSERAUTO)) pcUserAutoSelect(r , printWriter);
+                                if(sql.equals(SQL_DISTINCTIPSWITHINET)) {
+                                    printWriter.println(r.getString("ip"));
+                                }
+                                if(sql.contains("SELECT * FROM `inetstats` WHERE `ip` LIKE")) {
+                                    printWriter.print(new java.util.Date(Long.parseLong(r.getString("Date"))));
+                                    printWriter.print(",");
+                                    printWriter.print(r.getString(ConstantsFor.DBFIELB_RESPONSE));
+                                    printWriter.print(",");
+                                    printWriter.print(r.getString("bytes"));
+                                    printWriter.print(",");
+                                    printWriter.print(r.getString(ConstantsFor.DBFIELD_METHOD));
+                                    printWriter.print(",");
+                                    printWriter.print(r.getString("site"));
+                                    printWriter.println();
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            String msgTimeSp = new StringBuilder()
-                .append("WeekPCStats.getFromDB method. ")
-                .append(( float ) (System.currentTimeMillis() - stArt) / 1000)
-                .append(" sec spend\n")
-                .append(file.getAbsolutePath())
-                .append(" ")
-                .append(( float ) file.length() / ConstantsFor.KBYTE).toString();
-            messageToUser.warn(msgTimeSp);
-        }
-        catch(SQLException | IOException e){
-            messageToUser.infoNoTitles(e.getMessage());
+        }catch(SQLException | IOException e){
+            messageToUser.error(FileSystemWorker.error(getClass().getSimpleName() + ".selectFrom" , e));
         }
         String toCopy = "\\\\10.10.111.1\\Torrents-FTP\\" + file.getName();
         if(!ConstantsFor.thisPC().toLowerCase().contains("home")){
@@ -103,25 +125,31 @@ public class WeekPCStats implements Runnable, DataBaseRegSQL {
     }
 
 
-    private void cleanInetStatsRSet(ResultSet r , PrintWriter printWriter) throws SQLException {
-        printWriter.print(new Date(r.getLong("Date")));
-        printWriter.print(",");
-        printWriter.print(r.getString("ip"));
-        printWriter.print(",");
-        printWriter.print(r.getString(ConstantsFor.DBFIELB_RESPONSE));
-        printWriter.print(",");
-        printWriter.print(r.getString(ConstantsFor.DBFIELD_METHOD));
-        printWriter.print(",");
-        printWriter.print(r.getString("site"));
-        printWriter.println();
+    private void readInetStatsRSetToCSV() {
+        List<String> chkIps = FileSystemWorker.readFileToList(new File(FILENAME_INETSTATSIPCSV).getPath());
+        long totalBytes = 0;
+        for(String ip : chkIps){
+            File file = new File(fileName);
+            this.fileName = FILENAME_INETSTATSCSV.replace("inetstats" , ip).replace(".csv" , "_" + LocalTime.now().toSecondOfDay() + ".csv");
+            this.sql = "SELECT * FROM `inetstats` WHERE `ip` LIKE '" + ip + "'";
+            selectFrom();
+            totalBytes = totalBytes + file.length();
+            new MessageLocal(getClass().getSimpleName())
+                .info(fileName , file.length() / ConstantsFor.KBYTE + " kb" , "total megabytes: " + totalBytes / ConstantsFor.KBYTE / ConstantsFor.KBYTE);
+            if(file.length() > 10) {
+                this.sql = "DELETE FROM `inetstats` WHERE `ip` LIKE '" + ip + "'";
+                selectFrom();
+            }
+        }
+        new MessageSwing().infoTimer(ConstantsFor.ONE_DAY_HOURS * 3 , "ALL STATS SAVED\n" + totalBytes / ConstantsFor.KBYTE / ConstantsFor.KBYTE + " Megabytes");
     }
 
 
-    private long cleanInetStats() {
-        this.sql = "select * from inetstats";
-        this.fileName = FILENAME_INETSTATSCSV;
+    private long readInetStats() {
+        this.fileName = FILENAME_INETSTATSIPCSV;
+        this.sql = SQL_DISTINCTIPSWITHINET;
         selectFrom();
-        return new File(FILENAME_INETSTATSCSV).length() / ConstantsFor.KBYTE;
+        return new File(FILENAME_INETSTATSIPCSV).length() / ConstantsFor.KBYTE;
     }
 
 
