1839a85443226c2a910a6b071a20adda3ba25dc0
Vachok <143500@gmail.com>
2019-05-06 21:52:21 +0300

    feat(reverse nohup):
    2 minutes
    
    1 minute

--- modules/networker/src/main/java/ru/vachok/networker/controller/NoHupOut.java
+++ modules/networker/src/main/java/ru/vachok/networker/controller/NoHupOut.java
@@ -8,11 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.ConstantsFor;
+import ru.vachok.networker.TForms;
 import ru.vachok.networker.componentsrepo.PageFooter;
 import ru.vachok.networker.fileworks.FileSystemWorker;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -27,15 +31,18 @@ public class NoHupOut {
     private File noHup = new File("nohup.out");
     
     @GetMapping("/nohup")
-    public String noHupGet(Model model, HttpServletRequest request) {
+    public String noHupGet(Model model, HttpServletRequest request, HttpServletResponse response) {
         if (!noHup.exists()) {
             throw new UnsupportedOperationException(System.getProperty("os.name"));
         }
+        List<String> strings = FileSystemWorker.readFileToList(noHup.getAbsolutePath());
+        Collections.reverse(strings);
         new AppComponents().visitor(request);
         model.addAttribute(ConstantsFor.ATT_TITLE, System.getProperty("os.name"));
         model.addAttribute(ConstantsFor.ATT_HEAD, new PageFooter().getHeaderUtext());
-        model.addAttribute("ok", FileSystemWorker.readFile(noHup.getAbsolutePath()));
+        model.addAttribute("ok", new TForms().fromArray(strings, true));
         model.addAttribute(ConstantsFor.ATT_FOOTER, new PageFooter().getFooterUtext());
+        response.addHeader(ConstantsFor.HEAD_REFRESH, "15");
         return "ok";
     }
     
