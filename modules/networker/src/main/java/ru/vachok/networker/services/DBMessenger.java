353a7b73e43a57084af05ec78a4529c61e4289c2
Vachok <143500@gmail.com>
2019-06-08 16:09:02 +0300

    feat(dbmessenger from old branch):
    1 minute

--- modules/networker/src/main/java/ru/vachok/networker/services/DBMessenger.java
+++ modules/networker/src/main/java/ru/vachok/networker/services/DBMessenger.java
@@ -6,7 +6,7 @@ package ru.vachok.networker.services;
 import ru.vachok.messenger.MessageToUser;
 import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.ConstantsFor;
-import ru.vachok.networker.fileworks.FileSystemWorker;
+import ru.vachok.networker.exe.ThreadConfig;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -20,6 +20,8 @@ import java.sql.SQLException;
 public class DBMessenger implements MessageToUser {
     
     
+    private final ThreadConfig thrConfig = AppComponents.threadConfig();
+    
     private String headerMsg;
     
     private String titleMsg;
@@ -31,17 +33,23 @@ public class DBMessenger implements MessageToUser {
     }
     
     @Override public void errorAlert(String headerMsg, String titleMsg, String bodyMsg) {
-        this.headerMsg = headerMsg; this.titleMsg = titleMsg; this.bodyMsg = bodyMsg; dbSend(headerMsg, titleMsg, bodyMsg);
+        this.headerMsg = headerMsg;
+        this.titleMsg = titleMsg;
+        this.bodyMsg = bodyMsg;
+        Runnable errSend = ()->dbSend(headerMsg, titleMsg, bodyMsg);
+        thrConfig.execByThreadConfig(errSend);
     }
     
     @Override public void infoNoTitles(String s) {
-        dbSend(headerMsg, "INFO", s);
+        Runnable info = ()->dbSend(headerMsg, "INFO", s);
+        thrConfig.execByThreadConfig(info);
     }
 
 
     @Override
     public void info( String s , String s1 , String s2 ) {
-        dbSend(s,s1,s2);
+        final Runnable dbSendRun = ()->dbSend(s, s1, s2);
+        thrConfig.execByThreadConfig(dbSendRun);
     }
 
     @Override public void error(String s) {
@@ -57,24 +65,29 @@ public class DBMessenger implements MessageToUser {
         this.headerMsg = headerMsg; this.titleMsg = s1; this.bodyMsg = s2; errorAlert(headerMsg, s1, s2);
     }
     
-    private void dbSend(String headerMsg, String titleMsg, String bodyMsg) {
-        String sql = "insert into ru_vachok_networker (classname, msgtype, msgvalue) values (?,?,?)"; try (Connection c = new AppComponents().connection(ConstantsFor.DBPREFIX + "webapp");
-                                                                                                           PreparedStatement p = c.prepareStatement(sql)) {
-            p.setString(1, headerMsg); p.setString(2, titleMsg); p.setString(3, bodyMsg); p.executeUpdate();
-        }
-        catch (SQLException | IOException e) {
-            FileSystemWorker.error("DBMessenger.dbSend", e);
-        }
-    }
-
     @Override
-    public void infoTimer( int i , String s ) {
+    public void warn(String s, String s1, String s2) {
+        info(s, s1, s2);
+    }
+    
+    @Override
+    public void infoTimer(int i, String s) {
         throw new UnsupportedOperationException("07.09.2018 (0:11)");
     }
 
-    @Override
-    public void warn(String s, String s1, String s2) {
-        throw new UnsupportedOperationException();
+    private void dbSend(String headerMsg, String titleMsg, String bodyMsg) {
+        final String sql = "insert into ru_vachok_networker (classname, msgtype, msgvalue, pc) values (?,?,?,?)";
+        try (Connection c = new AppComponents().connection(ConstantsFor.DBPREFIX + "webapp");
+             PreparedStatement p = c.prepareStatement(sql)) {
+            p.setString(1, headerMsg);
+            p.setString(2, titleMsg);
+            p.setString(3, bodyMsg);
+            p.setString(4, ConstantsFor.thisPC() + " up: " + ConstantsFor.getUpTime());
+            p.executeUpdate();
+        }
+        catch (SQLException | IOException e) {
+            System.err.println(e.getMessage() + " " + getClass().getSimpleName() + ".dbSend");
+        }
     }
 
     @Override
