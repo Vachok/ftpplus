fa060950fae46c00006d6ebfbd521c478654220f
Vachok <143500@gmail.com>
2019-06-08 10:00:28 +0300

    fix:
    17 minutes
    
    6 minutes
    
    5 minutes

--- modules/networker/src/test/java/ru/vachok/networker/exe/runnabletasks/NetMonitorPTVTEST.java
+++ modules/networker/src/test/java/ru/vachok/networker/exe/runnabletasks/NetMonitorPTVTEST.java
@@ -3,6 +3,7 @@
 package ru.vachok.networker.exe.runnabletasks;
 
 
+import org.testng.annotations.Test;
 import ru.vachok.messenger.MessageToUser;
 import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.ConstantsFor;
@@ -25,29 +26,34 @@ import java.util.prefs.Preferences;
  Периодический мониторинг телевизоров и WiFi.
  <p>
  Точки : <br>
- {@link ru.vachok.networker.net.enums.SwitchesWiFi#C_204_2_UPAK} ; {@link ru.vachok.networker.net.enums.SwitchesWiFi#C_204_3_UPAK} ;
- {@link ru.vachok.networker.net.enums.SwitchesWiFi#C_204_10_GP} ; {@link ru.vachok.networker.net.enums.OtherKnownDevices}
+ {@link SwitchesWiFi#C_204_2_UPAK} ; {@link SwitchesWiFi#C_204_3_UPAK} ;
+ {@link SwitchesWiFi#C_204_10_GP} ; {@link OtherKnownDevices}
  
  @since 05.02.2019 (9:00) */
-public class NetMonitorPTV implements Runnable {
+@SuppressWarnings("ALL") public class NetMonitorPTVTEST implements Runnable {
     
     
     private PrintStream printStream;
     
-    private MessageToUser messageToUser = new DBMessenger(NetMonitorPTV.class.getSimpleName());
+    private MessageToUser messageToUser = new DBMessenger(NetMonitorPTVTEST.class.getSimpleName());
     
     private String pingResultLast = "No pings yet.";
     
-    private static final String CLASS_NAME = NetMonitorPTV.class.getSimpleName();
+    private static final String CLASS_NAME = NetMonitorPTVTEST.class.getSimpleName();
     
-    public NetMonitorPTV() {
-        try {
-            OutputStream outputStream = new FileOutputStream(ConstantsFor.FILENAME_PTV);
+    public NetMonitorPTVTEST() {
+        
+        try (OutputStream outputStream = new FileOutputStream(ConstantsFor.FILENAME_PTV)) {
             this.printStream = new PrintStream(outputStream, true);
         }
         catch (IOException e) {
             messageToUser.error(e.getMessage());
         }
+    }
+    
+    @Test
+    public void testRun() {
+        new NetMonitorPTV().run();
     }
     
     @Override
@@ -63,7 +69,7 @@ public class NetMonitorPTV implements Runnable {
     
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("NetMonitorPTV{");
+        final StringBuilder sb = new StringBuilder("NetMonitorPTVTEST{");
         sb.append("pingResultLast='").append(pingResultLast).append('\'');
         sb.append('}');
         return sb.toString();
@@ -76,8 +82,8 @@ public class NetMonitorPTV implements Runnable {
         try {
             preferences.sync();
             if (!ptvFile.exists()) {
-                preferences.put(ConstantsFor.FILENAME_PTV, new Date().toString());
                 Files.createFile(ptvFile.toPath());
+                preferences.put(ConstantsFor.FILENAME_PTV, new Date().toString());
             }
             else if (filePath.toAbsolutePath().normalize().toFile().isFile()) {
                 preferences.sync();
@@ -112,11 +118,8 @@ public class NetMonitorPTV implements Runnable {
         boolean isPingTvCopied = FileSystemWorker
             .copyOrDelFile(pingTv, ConstantsFor.FILESYSTEM_SEPARATOR + "lan" + ConstantsFor.FILESYSTEM_SEPARATOR + "tv_" + System.currentTimeMillis() / 1000 + ".ping", true);
         if (isPingTvCopied) {
-            this.printStream.close();
             AppComponents.getProps().setProperty(this.getClass().getSimpleName(), new Date().toString());
             AppComponents.threadConfig().thrNameSet(getClass().getSimpleName());
-            OutputStream outputStream = new FileOutputStream(ConstantsFor.FILENAME_PTV);
-            this.printStream = new PrintStream(outputStream, true);
             printStream.println(new File(ConstantsFor.FILENAME_PTV).getAbsolutePath() + " as at : " + new Date());
         }
         else {
@@ -126,24 +129,24 @@ public class NetMonitorPTV implements Runnable {
     
     private void pingIPTV() throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
-    
+        
         byte[] upakCisco2042b = InetAddress.getByName(SwitchesWiFi.C_204_2_UPAK).getAddress();
         byte[] upakCisco2043b = InetAddress.getByName(SwitchesWiFi.C_204_3_UPAK).getAddress();
         byte[] gpCisco20410b = InetAddress.getByName(SwitchesWiFi.C_204_3_UPAK).getAddress();
-    
+        
         InetAddress ptv1 = InetAddress.getByName(OtherKnownDevices.PTV1_EATMEAT_RU);
         InetAddress ptv2 = InetAddress.getByName(OtherKnownDevices.PTV2_EATMEAT_RU);
         
         InetAddress upakCisco2042 = InetAddress.getByAddress(upakCisco2042b);
         InetAddress upakCisco2043 = InetAddress.getByAddress(upakCisco2043b);
         InetAddress gpCisco20410 = InetAddress.getByAddress(gpCisco20410b);
-    
+        
         boolean ptv1Reachable = ptv1.isReachable(ConstantsFor.TIMEOUT_650);
         boolean ptv2Reachable = ptv2.isReachable(ConstantsFor.TIMEOUT_650);
         boolean upakCisco2042Reachable = upakCisco2042.isReachable(ConstantsFor.TIMEOUT_650);
         boolean upakCisco2043Reachable = upakCisco2043.isReachable(ConstantsFor.TIMEOUT_650);
         boolean gpCisco2042Reachable = gpCisco20410.isReachable(ConstantsFor.TIMEOUT_650);
-    
+        
         stringBuilder.append(ptv1);
         stringBuilder.append(" is ");
         stringBuilder.append(ptv1Reachable);
@@ -151,10 +154,10 @@ public class NetMonitorPTV implements Runnable {
         stringBuilder.append(ptv2);
         stringBuilder.append(" is ");
         stringBuilder.append(ptv2Reachable);
-    
+        
         stringBuilder.append("<br>");
         stringBuilder.append("\n***Wi-Fi points:");
-    
+        
         stringBuilder.append(upakCisco2042).append(" is ").append(upakCisco2042Reachable).append(", ");
         stringBuilder.append(upakCisco2043).append(" is ").append(upakCisco2043Reachable).append(", ");
         stringBuilder.append(gpCisco20410).append(" is ").append(gpCisco2042Reachable).append("<br>***");
