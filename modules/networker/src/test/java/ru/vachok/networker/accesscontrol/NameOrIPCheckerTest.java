12b74bd539f7cae3df05647d7a0556bf20444571
Vachok <143500@gmail.com>
2019-06-16 12:25:37 +0300

    test:
    4 minutes

--- modules/networker/src/test/java/ru/vachok/networker/accesscontrol/NameOrIPCheckerTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/accesscontrol/NameOrIPCheckerTest.java
@@ -3,9 +3,13 @@
 package ru.vachok.networker.accesscontrol;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.vachok.networker.ConstantsFor;
+import ru.vachok.networker.TForms;
+import ru.vachok.networker.net.enums.OtherKnownDevices;
 
-import java.awt.*;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import static org.testng.Assert.assertNull;
@@ -18,7 +22,14 @@ public class NameOrIPCheckerTest {
     
     @Test
     public void testCheckPat() {
-        throw new IllegalComponentStateException("15.06.2019 (17:36)");
+        try {
+            InetAddress inetAddress = new NameOrIPChecker(OtherKnownDevices.DO0213_KUDR.replace(ConstantsFor.DOMAIN_EATMEATRU, "")).resolveIP();
+            String ipString = inetAddress.toString();
+            Assert.assertTrue(ipString.contains(OtherKnownDevices.DO0213_KUDR));
+        }
+        catch (UnknownHostException e) {
+            assertNull(e, e.getMessage() + "\n" + new TForms().fromArray(e, false));
+        }
     }
     
     @Test
