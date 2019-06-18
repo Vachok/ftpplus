4df3f4e73f8854fb8c9e21755f1c29d61bd248a6
Vachok <143500@gmail.com>
2019-06-18 22:52:07 +0300

    test:
    16 minutes

--- modules/networker/src/test/java/ru/vachok/networker/sysinfo/VersionInfoTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/sysinfo/VersionInfoTest.java
@@ -5,6 +5,13 @@ package ru.vachok.networker.sysinfo;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.vachok.networker.ConstantsFor;
+import ru.vachok.networker.fileworks.FileSystemWorker;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 
 /**
@@ -14,35 +21,67 @@ public class VersionInfoTest {
     
     @Test
     public void testGetPropertiesFrom() {
+        throw new IllegalComponentStateException("18.06.2019 (22:35)");
     }
     
     @Test
     public void testSetPropertiesFrom() {
-    }
-    
-    @Test
-    public void testGetAppBuild() {
-    }
-    
-    @Test
-    public void testGetAppVersion() {
-    }
-    
-    @Test
-    public void testGetBuildTime() {
+        throw new IllegalComponentStateException("18.06.2019 (22:35)");
     }
     
     @Test
     public void testSetParams() {
         String setParamsString = new VersionInfo().setParams();
-        System.out.println(setParamsString);
-        Assert.assertTrue(setParamsString.contains("is SET"), setParamsString);
+    
+        Assert.assertTrue(setParamsString.contains(ConstantsFor.thisPC()), setParamsString);
     }
     
     @Test
     public void getParamsTEST() {
-        String getParamsStr = new VersionInfo().getParams();
-        System.out.println(getParamsStr);
-        Assert.assertTrue(getParamsStr.contains("is GET"), getParamsStr);
+        VersionInfo infoVers = new VersionInfo();
+        String getParamsStr = infoVers.getParams();
+        Path parentPath = Paths.get(".").toAbsolutePath().normalize().getParent();
+        String buildGradleString = "NO file Build Gradle";
+        try {
+            VersionInfoTest.BuildGradleSearch buildSearch = new VersionInfoTest.BuildGradleSearch();
+            Files.walkFileTree(parentPath, buildSearch);
+            Path pathBuildGradle = buildSearch.getBuildGradlePath();
+            Assert.assertNotNull(pathBuildGradle, parentPath.toString());
+            buildGradleString = FileSystemWorker.readFile(pathBuildGradle.toAbsolutePath().normalize().toString());
+        }
+        catch (IOException e) {
+            Assert.assertNull(e, e.getMessage() + " \n" + parentPath);
+        }
+    
+        Assert.assertTrue(buildGradleString.contains(infoVers.getAppVersion()), infoVers.getAppVersion() + "\n" + buildGradleString);
+    }
+    
+    private class BuildGradleSearch extends SimpleFileVisitor<Path> {
+        
+        
+        private Path buildGradlePath;
+        
+        public Path getBuildGradlePath() {
+            return buildGradlePath;
+        }
+        
+        @Override public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+        
+        @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (file.toFile().getName().toLowerCase().contains(ConstantsFor.FILENAME_BUILDGRADLE) & attrs.isRegularFile()) {
+                this.buildGradlePath = file.toAbsolutePath().normalize();
+            }
+            return FileVisitResult.CONTINUE;
+        }
+        
+        @Override public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+        
+        @Override public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
