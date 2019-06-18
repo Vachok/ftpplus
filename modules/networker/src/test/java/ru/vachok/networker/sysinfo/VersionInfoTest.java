85e3338e6cb250b20dfd76585bacd5b708f7a069
Vachok <143500@gmail.com>
2019-06-19 01:00:32 +0300

    test:
    32 seconds

--- modules/networker/src/test/java/ru/vachok/networker/sysinfo/VersionInfoTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/sysinfo/VersionInfoTest.java
@@ -7,11 +7,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.ConstantsFor;
-import ru.vachok.networker.fileworks.FileSystemWorker;
-
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 
 
 /**
@@ -30,47 +25,5 @@ import java.nio.file.attribute.BasicFileAttributes;
         String versString = infoVers.toString();
         Assert.assertFalse(versString.contains("rups00"), versString);
         Assert.assertEquals(AppComponents.getUserPref().get(ConstantsFor.PR_APP_VERSION, ""), infoVers.getAppVersion());
-        Path parentPath = Paths.get(".").toAbsolutePath().normalize().getParent();
-        String buildGradleString = "NO file Build Gradle";
-        try {
-            VersionInfoTest.BuildGradleSearch buildSearch = new VersionInfoTest.BuildGradleSearch();
-            Files.walkFileTree(parentPath, buildSearch);
-            Path pathBuildGradle = buildSearch.getBuildGradlePath();
-            Assert.assertNotNull(pathBuildGradle, parentPath.toString());
-            buildGradleString = FileSystemWorker.readFile(pathBuildGradle.toAbsolutePath().normalize().toString());
-        }
-        catch (IOException e) {
-            Assert.assertNull(e, e.getMessage() + " \n" + parentPath);
-        }
-        Assert.assertTrue(buildGradleString.contains(infoVers.getAppVersion()), infoVers.getAppVersion() + "\n" + buildGradleString);
-    }
-    
-    private class BuildGradleSearch extends SimpleFileVisitor<Path> {
-        
-        
-        private Path buildGradlePath;
-        
-        public Path getBuildGradlePath() {
-            return buildGradlePath;
-        }
-        
-        @Override public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-            return FileVisitResult.CONTINUE;
-        }
-        
-        @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (file.toFile().getName().toLowerCase().contains(ConstantsFor.FILENAME_BUILDGRADLE) & attrs.isRegularFile()) {
-                this.buildGradlePath = file.toAbsolutePath().normalize();
-            }
-            return FileVisitResult.CONTINUE;
-        }
-        
-        @Override public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-            return FileVisitResult.CONTINUE;
-        }
-        
-        @Override public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-            return FileVisitResult.CONTINUE;
-        }
     }
 }
