c002592ee59a5b8b948506b26cf91089653fd30a
Vachok <143500@gmail.com>
2019-06-21 12:09:19 +0300

    fix(InetStatsSorter):

--- modules/networker/src/main/java/ru/vachok/networker/accesscontrol/inetstats/InetStatSorter.java
+++ modules/networker/src/main/java/ru/vachok/networker/accesscontrol/inetstats/InetStatSorter.java
@@ -9,6 +9,10 @@ import ru.vachok.networker.fileworks.FileSystemWorker;
 import ru.vachok.networker.services.MessageLocal;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -17,7 +21,8 @@ import java.util.*;
  Class ru.vachok.networker.accesscontrol.inetstats.InetStatSorter
  <p>
  Устойчивость - 1/(1+1). 100%.
-
+ 
+ @see ru.vachok.networker.accesscontrol.inetstats.InetStatSorterTest
  @since 14.04.2019 (4:09) */
 public class InetStatSorter implements Runnable {
 
@@ -28,9 +33,102 @@ public class InetStatSorter implements Runnable {
         sortFiles();
     }
     
-    /**
-     Поиск файлов .csv в папке запуска.
-     */
+    public void sortFiles() {
+        File[] rootFiles = new File(".").listFiles();
+        Map<File, String> mapFileStringIP = new TreeMap<>();
+        Set<String> ipsSet = new TreeSet<>();
+        
+        for (File fileFromRoot : Objects.requireNonNull(rootFiles)) {
+            if (fileFromRoot.getName().toLowerCase().contains(".csv")) {
+                try {
+                    String[] nameSplit = fileFromRoot.getName().split("_");
+                    mapFileStringIP.put(fileFromRoot, nameSplit[0].replace(".csv", ""));
+                }
+                catch (ArrayIndexOutOfBoundsException ignore) {
+                    //
+                }
+            }
+        }
+        if (mapFileStringIP.size() == 0) {
+            FileSystemWorker.writeFile("no.csv", new Date().toString());
+        }
+        else {
+            mapFileStringIP.values().stream().forEach(ipAddr->ipsSet.add(ipAddr));
+            ipsSet.forEach(ip->{
+                Queue<File> csvTMPFilesQueue = new LinkedList<>();
+                for (File file : mapFileStringIP.keySet()) {
+                    if (file.getName().contains("_") & file.getName().contains(ip)) {
+                        csvTMPFilesQueue.add(file);
+                    }
+                }
+                makeCSV(ip, csvTMPFilesQueue);
+            });
+            FileSystemWorker.writeFile("inetips.set", ipsSet.stream());
+        }
+    }
+    
+    @Override public String toString() {
+        final StringBuilder sb = new StringBuilder("InetStatSorter{");
+        sb.append(LocalDate.now().getDayOfWeek());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    private void makeCSV(String ip, Queue<File> queueCSVFilesFromRoot) {
+        String fileSepar = System.getProperty("file.separator");
+        String pathInetStats = Paths.get(".").toAbsolutePath().normalize() + fileSepar + "inetstats" + fileSepar;
+        File finalFile = new File(pathInetStats + ip + ".csv");
+        
+        Set<String> toWriteStatsSet = new TreeSet<>();
+        
+        if (finalFile.exists() & queueCSVFilesFromRoot.size() > 0) {
+            toWriteStatsSet.addAll(FileSystemWorker.readFileToSet(finalFile.toPath()));
+        }
+        if (queueCSVFilesFromRoot.size() > 0) {
+            System.out.println("Adding statistics to: " + finalFile.getAbsolutePath());
+            Iterator<File> fileIterator = queueCSVFilesFromRoot.iterator();
+            while (fileIterator.hasNext()) {
+                File nextFile = fileIterator.next();
+                toWriteStatsSet.addAll(FileSystemWorker.readFileToSet(nextFile.toPath()));
+                nextFile.deleteOnExit();
+            }
+            boolean isWrite = FileSystemWorker.writeFile(finalFile.getAbsolutePath(), toWriteStatsSet.stream());
+            System.out.println(isWrite);
+        }
+        else {
+            System.out.println(finalFile.getAbsolutePath() + " is NOT modified.");
+        }
+    }
+    
+    private void copyToFolder(File file) {
+        String absPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        
+        String fileSepar = System.getProperty("file.separator");
+        File inetStatsDir = new File(absPath + fileSepar + "inetstats");
+        boolean isDirExist = inetStatsDir.isDirectory();
+        
+        if (!isDirExist) {
+            try {
+                Files.createDirectories(inetStatsDir.toPath());
+            }
+            catch (IOException e) {
+                messageToUser.error(e.getMessage());
+            }
+        }
+        
+        try {
+            Path copyPath = Files.copy(Paths.get(absPath + fileSepar + file.getName()), file.toPath());
+            if (file.equals(copyPath.toFile())) {
+                new File(file.getAbsolutePath().replace(fileSepar + "inetstats" + fileSepar, fileSepar)).deleteOnExit();
+            }
+        }
+        catch (IOException e) {
+            messageToUser.error(e.getMessage());
+        }
+        
+    }
+    
+    /* 21.06.2019 (12:07)
     private void sortFiles() {
         File[] rootFiles = new File(".").listFiles();
         Map<String, File> ipsFromFiles = new HashMap<>();
@@ -77,5 +175,5 @@ public class InetStatSorter implements Runnable {
             messageToUser.info(tmpInetStatFile.getAbsolutePath(), "exist: " + true, " To write =  " + fileAsSet.size() + " strings");
             FileSystemWorker.writeFile(tmpInetStatFile.getName(), fileAsSet.stream());
         }
-    }
+    }*/
 }
