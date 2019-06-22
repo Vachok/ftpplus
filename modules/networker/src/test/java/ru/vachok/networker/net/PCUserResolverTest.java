11aa8c0447e7d13807672cd72bd82ccda7e4940d
Vachok <143500@gmail.com>
2019-06-22 22:57:34 +0300

    chore:
    34 seconds

--- modules/networker/src/test/java/ru/vachok/networker/net/PCUserResolverTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/net/PCUserResolverTest.java
@@ -1,3 +1,5 @@
+// Copyright (c) all rights. http://networker.vachok.ru 2019.
+
 package ru.vachok.networker.net;
 
 
@@ -11,7 +13,7 @@ import org.testng.annotations.Test;
     @Test
     public void testRun() {
         PCUserResolver pcUserResolver = new PCUserResolver("do0213");
-        pcUserResolver.setInfo();
+        pcUserResolver.setInfo(); //todo 22.06.2019 (22:57) Проверка на доступность хоста.
         String resolverInfoAbout = pcUserResolver.getInfoAbout();
         Assert.assertFalse(resolverInfoAbout.isEmpty(), resolverInfoAbout);
     }
