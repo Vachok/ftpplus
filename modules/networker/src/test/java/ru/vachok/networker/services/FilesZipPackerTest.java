347b5f658043fa9d887741656d09ae731c7fc7a7
Vachok <143500@gmail.com>
2019-06-22 06:51:49 +0300

    test:
    файл больше не перепиывается. но и не ищет.
    
    1 hour 35 minutes

--- modules/networker/src/test/java/ru/vachok/networker/services/FilesZipPackerTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/services/FilesZipPackerTest.java
@@ -6,12 +6,11 @@ package ru.vachok.networker.services;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.vachok.networker.ConstantsFor;
+import ru.vachok.networker.TForms;
 
 import java.io.*;
 import java.nio.file.attribute.FileTime;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -32,10 +31,15 @@ import java.util.zip.ZipOutputStream;
         catch (Exception e) {
             Assert.assertNotNull(e);
         }
-        System.out.println("callCopy() = " + callCopy());
+        try {
+            System.out.println("callCopy() = " + callCopy());
+        }
+        catch (IOException e) {
+            Assert.assertNull(e, e.getMessage() + "\n" + new TForms().fromArray(e, false));
+        }
     }
     
-    private String callCopy() {
+    private String callCopy() throws IOException {
         String retString = "null";
         try {
             retString = zipFilesMakerCopy();
@@ -61,7 +65,7 @@ import java.util.zip.ZipOutputStream;
         }
     }
     
-    private String zipFilesMakerCopy() {
+    private String zipFilesMakerCopy() throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
     
         File[] inetStatsFiles = new File("inetstats").listFiles();
@@ -74,14 +78,7 @@ import java.util.zip.ZipOutputStream;
         }
         stringBuilder.append(inetStatsFiles.length).append(" files of statistics. Total size: ").append(statFilesSize / ConstantsFor.MBYTE).append(" megabytes.\n");
         System.out.println(stringBuilder.toString());
-        try {
-            stringBuilder.append(createNEWZip(Arrays.asList(inetStatsFiles)));
-        }
-        catch (IOException e) {
-            stringBuilder.append(e.getMessage());
-            Assert.assertNull(e);
-            Assert.assertTrue(new File("stats.zip").delete());
-        }
+        stringBuilder.append(createNEWZip(Arrays.asList(inetStatsFiles)));
         return stringBuilder.toString();
     }
     
@@ -90,78 +87,81 @@ import java.util.zip.ZipOutputStream;
         File zipFileRaw = new File("stats.zip");
         boolean isExists = zipFileRaw.exists();
         if (!isExists) {
-            try (OutputStream outputStream = new FileOutputStream(zipFileRaw);
-                 ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
-                int numFiles = toPackInZipFilesList.size();
-                for (File file : toPackInZipFilesList) {
-                    byte[] bufBytes = new byte[ConstantsFor.KBYTE];
-                    zipOutputStream.putNextEntry(new ZipEntry(file.getPath()).setLastModifiedTime(FileTime.fromMillis(file.lastModified())));
-                    int inRead;
-                    InputStream inputStream = new FileInputStream(file.getAbsolutePath());
-                    while ((inRead = inputStream.read(bufBytes)) > 0) {
-                        zipOutputStream.write(bufBytes, 0, inRead);
-                    }
-                    inputStream.close();
-                
-                    String progressString = new StringBuilder().append(zipFileRaw.length() / ConstantsFor.MBYTE)
-                        .append(" mbytes in: ").append(zipFileRaw.getAbsolutePath())
-                        .append(" file last packet is: ").append(file.getName())
-                        .append(" left: ").append(--numFiles)
-                        .append(" files").toString();
-                
-                    System.out.println(progressString);
-                    stringBuilder.append(progressString);
-                }
-            }
-            Assert.assertTrue(zipFileRaw.length() > 10);
+            writeToFile(zipFileRaw, toPackInZipFilesList, stringBuilder);
         }
         else {
-            changeExistZip(toPackInZipFilesList);
+            changeExistZip(toPackInZipFilesList, zipFileRaw);
         }
         return stringBuilder.toString();
     }
     
-    private void changeExistZip(List<File> toPackInZipFilesList) throws IOException {
+    private void writeToFile(File zipFileRaw, List<?> toPackInZipFilesList, StringBuilder stringBuilder) {
+        try (OutputStream outputStream = new FileOutputStream(zipFileRaw);
+             ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
+            int numFiles = toPackInZipFilesList.size();
+            for (Object obj : toPackInZipFilesList) {
+                File file = null;
+                ZipEntry zipEntry = null;
+                if (obj instanceof File) {
+                    file = (File) obj;
+                }
+                else if (obj instanceof ZipEntry) {
+                    zipEntry = (ZipEntry) obj;
+                }
+                byte[] bufBytes = new byte[ConstantsFor.KBYTE];
+                zipEntry = new ZipEntry(file.getPath());
+                zipOutputStream.putNextEntry(zipEntry.setLastModifiedTime(FileTime.fromMillis(file.lastModified())));
+                int inRead;
+                InputStream inputStream = new FileInputStream(file.getAbsolutePath());
+                while ((inRead = inputStream.read(bufBytes)) > 0) {
+                    zipOutputStream.write(bufBytes, 0, inRead);
+                }
+                inputStream.close();
+                
+                String progressString = new StringBuilder().append(zipFileRaw.length() / ConstantsFor.MBYTE)
+                    .append(" mbytes in: ").append(zipFileRaw.getAbsolutePath())
+                    .append(" file last packet is: ").append(file.getName())
+                    .append(" left: ").append(--numFiles)
+                    .append(" files").toString();
+                
+                System.out.println(progressString);
+                stringBuilder.append(progressString);
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage() + " " + getClass().getSimpleName() + ".writeToFile");
+        }
+        Assert.assertTrue(zipFileRaw.length() > 10);
+    }
+    
+    private void changeExistZip(List<File> toPackInZipFilesList, File zipFileRaw) throws IOException {
         ZipFile zipFile = new ZipFile("stats.zip");
         Enumeration<? extends ZipEntry> inZipEntries = zipFile.entries();
-        Queue<File> toChangeInZip = new LinkedList<>();
+        List<String> fileNames = new ArrayList<>();
         
-        while (inZipEntries.hasMoreElements()) {
-            ZipEntry zipEntry = inZipEntries.nextElement();
-            Predicate<File> predicate = file->zipEntry.getName().contains(file.getName()) & (zipEntry.getTime() < file.lastModified());
-            toPackInZipFilesList.stream().anyMatch(predicate.and(file->toChangeInZip.add(file)));
+        toPackInZipFilesList.stream().forEach(x->fileNames.add(x.getPath()));
+        List<ZipEntry> listEnt = new ArrayList<>();
+        ArrayList<? extends ZipEntry> list = Collections.list(inZipEntries);
+        list.forEach(ent->listEnt.add((ZipEntry) ent));
+        int indexEnt = 0;
+        for (ZipEntry zipEntry : listEnt) {
+            indexEnt = Collections.binarySearch(fileNames, zipEntry.getName());
         }
-        Assert.assertTrue(toChangeInZip.size() > 0);
-        throw new IllegalStateException("22.06.2019 (2:57)");
-    }
-    
-    private void makeChangesInZip(ZipFile zipFile, Queue<File> zipRenewQueue) throws IOException {
-        while (zipRenewQueue.iterator().hasNext()) {
-            File changedFile = zipRenewQueue.poll();
-            ZipEntry zipEntry = zipFile.getEntry(changedFile.getPath());
-            Assert.assertNotNull(zipEntry);
-            zipEntry = new ZipEntry(changedFile.getPath());
-            if (changedFile.lastModified() > zipEntry.getTime()) {
-                try (InputStream inputStream = new FileInputStream(changedFile);
-                     OutputStream outputStream = new FileOutputStream(zipFile.getName());
-                     ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
-                    zipOutputStream.putNextEntry(zipEntry);
-                    byte[] bytesBuff = new byte[ConstantsFor.KBYTE];
-                    int inRead;
-                    while ((inRead = inputStream.read(bytesBuff)) > 0) {
-                        zipOutputStream.write(bytesBuff, 0, inRead);
-                    }
-                }
-                Assert.assertTrue(new File(zipFile.getName()).lastModified() > System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(30));
-                System.out.println(new Date(zipEntry.getTime()) + " in zip" + zipEntry.getName() + " FILE IS CHANGED " + new Date(changedFile.lastModified()) + " is " + changedFile
-                    .getAbsolutePath());
-            }
-            else {
-                Assert.assertTrue(new File(zipFile.getName()).lastModified() < System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(30));
-                System.out.println(new Date(zipEntry.getTime()) + " in zip" + zipEntry.getName() + " FILE IS NOT CHANGED " + new Date(changedFile.lastModified()) + " is " + changedFile
-                    .getAbsolutePath());
-            }
+        
+        if (indexEnt != 0) {
+            ZipEntry entry = listEnt.get(indexEnt);
+            System.out.println(entry.getName());
+            ZipEntry zipEntry = new ZipEntry(new File(entry.getName()).getPath());
+            listEnt.add(zipEntry);
+        }
+        else {
+            System.out.println("NO CHANGES NEED!");
         }
     }
     
+    private void createNEWZipEntry(List<?> zipEntries) {
+        File zipFileRaw = new File("stats.zip");
+        zipFileRaw.delete();
+        this.writeToFile(zipFileRaw, zipEntries, new StringBuilder());
+    }
 }
