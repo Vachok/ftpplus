42ec782ab98841df883e62fa638301b36241dc97
Vachok <143500@gmail.com>
2019-07-10 12:14:59 +0300

    test:

--- modules/networker/src/test/java/ru/vachok/networker/services/DBMessengerTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/services/DBMessengerTest.java
@@ -4,8 +4,14 @@ package ru.vachok.networker.services;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.vachok.messenger.MessageToUser;
+import ru.vachok.networker.AppComponents;
+import ru.vachok.networker.TForms;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,8 +25,34 @@ public class DBMessengerTest {
     
     @Test
     public void sendMessage() {
+        final String sql = "SELECT * FROM `ru_vachok_networker` ORDER BY `ru_vachok_networker`.`counter` DESC LIMIT 1";
+        
         messageToUser.info(getClass().getSimpleName());
+    
+        Assert.assertTrue(checkMessageExistsInDatabase(sql));
+        
         checkFile();
+    }
+    
+    private boolean checkMessageExistsInDatabase(String sql) {
+        String dbName = "u0466446_webapp";
+        int executePS = 0;
+        
+        try (Connection c = new AppComponents().connection(dbName);
+             PreparedStatement p = c.prepareStatement(sql);
+             ResultSet resultSet = p.executeQuery();
+        ) {
+            while (resultSet.next()) {
+                String bodyMsg = resultSet.getString("msgvalue");
+                Assert.assertEquals(bodyMsg, getClass().getSimpleName(), resultSet.getString("pc"));
+                executePS = resultSet.getInt("counter");
+            }
+        }
+        catch (SQLException e) {
+            Assert.assertNull(e, e.getMessage() + "\n" + new TForms().fromArray(e));
+        }
+        System.out.println("Records counter = " + executePS);
+        return executePS > 0;
     }
     
     private void checkFile() {
