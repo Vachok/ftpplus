765a883ded1cb4cd0487dbbcce05062f5c6f2b7c
Vachok <143500@gmail.com>
2019-10-12 19:11:50 +0300

    test:
    1 minute

--- modules/networker/src/test/java/ru/vachok/networker/restapi/message/DBMessengerTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/restapi/message/DBMessengerTest.java
@@ -4,8 +4,11 @@ package ru.vachok.networker.restapi.message;
 
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import ru.vachok.networker.AbstractForms;
+import ru.vachok.networker.AppComponents;
 import ru.vachok.networker.TForms;
 import ru.vachok.networker.configuretests.TestConfigure;
 import ru.vachok.networker.configuretests.TestConfigureThreadsLogMaker;
@@ -13,9 +16,14 @@ import ru.vachok.networker.data.enums.ConstantsFor;
 import ru.vachok.networker.restapi.database.DataConnectTo;
 
 import java.sql.*;
-import java.text.*;
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 
 /**
@@ -53,7 +61,17 @@ public class DBMessengerTest {
     @Test
     public void testWork() {
         new DBMessenger(DBMessengerTest.class.getSimpleName()).info("test", "test", "test");
-        checkMessageExistsInDatabase();
+        Future<?> checkDBFuture = AppComponents.threadConfig().getTaskExecutor().getThreadPoolExecutor().submit(()->checkMessageExistsInDatabase());
+        try {
+            checkDBFuture.get(20, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().checkAccess();
+            Thread.currentThread().interrupt();
+        }
+        catch (ExecutionException | TimeoutException e) {
+            Assert.assertNull(e, e.getMessage() + "\n" + new TForms().fromArray(e));
+        }
     }
     
     private void checkMessageExistsInDatabase() {
