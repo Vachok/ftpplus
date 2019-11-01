56ad3c4d0c77be7ed59b8d1ae4c431d40bca7229
Vachok <143500@gmail.com>
2019-11-01 23:50:02 +0300

    test:
    24 minutes

--- modules/networker/src/main/java/ru/vachok/networker/restapi/database/H2DB.java
+++ modules/networker/src/main/java/ru/vachok/networker/restapi/database/H2DB.java
@@ -2,6 +2,7 @@ package ru.vachok.networker.restapi.database;
 
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.jetbrains.annotations.NotNull;
 import ru.vachok.networker.AbstractForms;
 import ru.vachok.networker.componentsrepo.exceptions.TODOException;
 import ru.vachok.networker.restapi.message.MessageToUser;
@@ -20,6 +21,18 @@ public class H2DB implements DataConnectTo {
     
     private static final MessageToUser messageToUser = MessageToUser.getInstance(MessageToUser.LOCAL_CONSOLE, H2DB.class.getSimpleName());
     
+    
+    static {
+        try {
+            Driver driver = new org.h2.Driver();
+            DriverManager.registerDriver(driver);
+        }
+        catch (SQLException e) {
+            messageToUser.error(MessageFormat.format("H2DB.static initializer", e.getMessage(), AbstractForms.exceptionNetworker(e.getStackTrace())));
+        }
+        
+    }
+    
     @Override
     public MysqlDataSource getDataSource() {
         throw new UnsupportedOperationException(toString());
@@ -36,28 +49,33 @@ public class H2DB implements DataConnectTo {
     }
     
     @Override
-    public int createTable(String dbPointTable, List<String> additionalColumns) {
-        throw new TODOException("01.11.2019 (9:24)");
-    }
-    
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("H2DB{");
-        sb.append('}');
-        return sb.toString();
-    }
-    
-    static {
-        try {
-            Driver driver = new org.h2.Driver();
-            DriverManager.registerDriver(driver);
+    public int createTable(@NotNull String dbPointTable, @NotNull List<String> additionalColumns) {
+        if (dbPointTable.contains(".")) {
+            dbPointTable = dbPointTable.split("\\Q.\\E")[1];
+        }
+        StringBuilder preSQL = new StringBuilder()
+            .append("CREATE TABLE ").append(dbPointTable).append(" (\n")
+            .append("\t`idRec` INT NOT NULL AUTO_INCREMENT,\n\t`tstamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),\n");
+        if (additionalColumns.size() > 0) {
+            for (String collumn : additionalColumns) {
+                preSQL.append("\t");
+                preSQL.append(collumn);
+                preSQL.append(",\n");
+            }
+        }
+        preSQL.append("\tPRIMARY KEY (`idRec`)")
+            .append(");");
+        final String sql = preSQL.toString();
+        try (Connection connection = getDefaultConnection(dbPointTable)) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                return preparedStatement.executeUpdate();
+            }
         }
         catch (SQLException e) {
-            messageToUser.error(MessageFormat.format("H2DB.static initializer", e.getMessage(), AbstractForms.exceptionNetworker(e.getStackTrace())));
+            messageToUser.error("H2DB.createTable", e.getMessage(), AbstractForms.exceptionNetworker(e.getStackTrace()));
+            return -666;
         }
-        
     }
-    
     
     @SuppressWarnings({"resource", "JDBCResourceOpenedButNotSafelyClosed"})
     @Override
@@ -70,5 +88,12 @@ public class H2DB implements DataConnectTo {
             messageToUser.error(MessageFormat.format("H2DB.getDefaultConnection", e.getMessage(), AbstractForms.exceptionNetworker(e.getStackTrace())));
         }
         return connection;
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("H2DB{");
+        sb.append('}');
+        return sb.toString();
     }
 }
--- modules/networker/src/test/java/ru/vachok/networker/restapi/database/H2DBTest.java
+++ modules/networker/src/test/java/ru/vachok/networker/restapi/database/H2DBTest.java
@@ -2,14 +2,24 @@ package ru.vachok.networker.restapi.database;
 
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import ru.vachok.networker.AbstractForms;
+import ru.vachok.networker.componentsrepo.exceptions.TODOException;
 import ru.vachok.networker.componentsrepo.fileworks.FileSystemWorker;
 import ru.vachok.networker.configuretests.TestConfigure;
 import ru.vachok.networker.configuretests.TestConfigureThreadsLogMaker;
 import ru.vachok.networker.data.DatabaseCleanerFromDuplicatesTest;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -17,6 +27,8 @@ import java.sql.*;
  @since 01.11.2019 (9:40) */
 public class H2DBTest {
     
+    
+    private H2DB h2DB;
     
     private static final TestConfigure TEST_CONFIGURE_THREADS_LOG_MAKER = new TestConfigureThreadsLogMaker(DatabaseCleanerFromDuplicatesTest.class
             .getSimpleName(), System.nanoTime());
@@ -30,6 +42,11 @@ public class H2DBTest {
     @AfterClass
     public void tearDown() {
         TEST_CONFIGURE_THREADS_LOG_MAKER.after();
+    }
+    
+    @BeforeMethod
+    public void initH2() {
+        this.h2DB = new H2DB();
     }
     
     @Test
@@ -78,6 +95,60 @@ public class H2DBTest {
         catch (SQLException e) {
             String messageErr = e.getMessage() + "\n" + AbstractForms.exceptionNetworker(e.getStackTrace());
             Assert.assertNull(e, messageErr);
+        }
+    }
+    
+    @Test
+    public void testGetDataSource() {
+        try {
+            h2DB.getDataSource();
+        }
+        catch (UnsupportedOperationException e) {
+            Assert.assertNotNull(e, e.getMessage() + "\n" + AbstractForms.fromArray(e));
+        }
+    }
+    
+    @Test
+    public void testUploadCollection() {
+        try {
+            h2DB.uploadCollection(Collections.singleton("test"), "test");
+        }
+        catch (TODOException e) {
+            Assert.assertNotNull(e, e.getMessage() + "\n" + AbstractForms.fromArray(e));
+        }
+    }
+    
+    @Test
+    public void testDropTable() {
+        try {
+            h2DB.dropTable("test");
+        }
+        catch (TODOException e) {
+            Assert.assertNotNull(e, e.getMessage() + "\n" + AbstractForms.fromArray(e));
+        }
+    }
+    
+    @Test
+    public void testCreateTable() {
+        List<String> colList = new ArrayList<>();
+        colList.add("`upstring` VARCHAR(100) NOT NULL DEFAULT 'not set'");
+        int tableCreate = h2DB.createTable("test.test", colList);
+        Assert.assertTrue(tableCreate == 0);
+    }
+    
+    @Test
+    public void testTestToString() {
+        String toStr = h2DB.toString();
+        Assert.assertEquals(toStr, "H2DB{}");
+    }
+    
+    @Test
+    public void testGetDefaultConnection() {
+        try (Connection connection = h2DB.getDefaultConnection("test.test")) {
+            Assert.assertTrue(connection.isValid(5));
+        }
+        catch (SQLException e) {
+            Assert.assertNull(e, e.getMessage() + "\n" + AbstractForms.fromArray(e));
         }
     }
     
