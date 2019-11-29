3f33d707b8225c8f422659b993274e47b7c791f4
Vachok <143500@gmail.com>
2019-11-29 22:54:40 +0300

    feat(time in message):
    1 minute

--- modules/networker/src/main/java/ru/vachok/networker/IntoApplication.java
+++ modules/networker/src/main/java/ru/vachok/networker/IntoApplication.java
@@ -22,10 +22,7 @@ import java.awt.*;
 import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Optional;
-import java.util.StringJoiner;
+import java.util.*;
 
 
 /**
@@ -35,19 +32,19 @@ import java.util.StringJoiner;
 @EnableScheduling
 @EnableAutoConfiguration
 public class IntoApplication {
-    
-    
+
+
     /**
      {@link MessageLocal}
      */
     private static final MessageToUser MESSAGE_LOCAL = MessageToUser.getInstance(MessageToUser.LOCAL_CONSOLE, IntoApplication.class.getSimpleName());
-    
+
     private static final boolean IS_TRAY_SUPPORTED = SystemTray.isSupported();
-    
+
     private static final SpringApplication SPRING_APPLICATION = new SpringApplication(IntoApplication.class);
-    
+
     private static ConfigurableApplicationContext configurableApplicationContext = SPRING_APPLICATION.run(IntoApplication.class);
-    
+
     @Contract(pure = true)
     public static ConfigurableApplicationContext getConfigurableApplicationContext() {
         FileSystemWorker.writeFile(IntoApplication.class.getSimpleName() + "." + configurableApplicationContext.hashCode(), AbstractForms
@@ -60,7 +57,7 @@ public class IntoApplication {
             return configurableApplicationContext;
         }
     }
-    
+
     public static void main(@NotNull String[] args) {
         Thread.currentThread().setName(IntoApplication.class.getSimpleName());
         setUTF8Enc();
@@ -75,25 +72,9 @@ public class IntoApplication {
             checkTray();
         }
     }
-    
-    protected static void setUTF8Enc() {
-        @NotNull StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault())).append("\n\n");
-        System.setProperty(PropertiesNames.ENCODING, "UTF8");
-        stringBuilder.append(AbstractForms.fromArray(System.getProperties()));
-        stringBuilder.append("http://").append(new NameOrIPChecker(UsefulUtilities.thisPC()).resolveInetAddress().getHostAddress()).append(":8880/");
-        MessageToUser.getInstance(MessageToUser.EMAIL, IntoApplication.class.getSimpleName())
-            .info(UsefulUtilities.thisPC(), "appInfoStarter", stringBuilder.toString());
-    }
-    
-    @Override
-    public String toString() {
-        return new StringJoiner(",\n", IntoApplication.class.getSimpleName() + "[\n", "\n]")
-            .toString();
-    }
-    
+
     static void checkTray() {
-    
+
         Optional optionalTray = SystemTrayHelper.getI();
         try {
             if (IS_TRAY_SUPPORTED && optionalTray.isPresent()) {
@@ -108,11 +89,29 @@ public class IntoApplication {
             appInfoStarter();
         }
     }
-    
+
+    @Override
+    public String toString() {
+        return new StringJoiner(",\n", IntoApplication.class.getSimpleName() + "[\n", "\n]")
+            .toString();
+    }
+
+    protected static void setUTF8Enc() {
+        @NotNull StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault())).append("\n\n");
+        System.setProperty(PropertiesNames.ENCODING, "UTF8");
+        stringBuilder.append(AbstractForms.fromArray(System.getProperties()));
+        stringBuilder.append("http://").append(new NameOrIPChecker(UsefulUtilities.thisPC()).resolveInetAddress().getHostAddress()).append(":8880/");
+        stringBuilder.append("\n\nSystem time: ").append(new Date(System.currentTimeMillis())).append(" atom time: ")
+            .append(new Date(UsefulUtilities.getAtomicTime())).append("\n\n");
+        MessageToUser.getInstance(MessageToUser.EMAIL, IntoApplication.class.getSimpleName())
+            .info(UsefulUtilities.thisPC(), "appInfoStarter", stringBuilder.toString());
+    }
+
     static void appInfoStarter() {
         @NotNull Runnable infoAndSched = new AppInfoOnLoad();
         AppComponents.threadConfig().getTaskExecutor().execute(infoAndSched, 50);
         MessageToUser.getInstance(MessageToUser.EMAIL, IntoApplication.class.getSimpleName()).info(MessageFormat
-                .format("{0} is {1}", configurableApplicationContext.getDisplayName(), configurableApplicationContext.isActive()));
+            .format("{0} is {1}", configurableApplicationContext.getDisplayName(), configurableApplicationContext.isActive()));
     }
 }
