8f766341a2a0f0b37e86664459ed21775a01bcb8
Vachok <143500@gmail.com>
2019-12-15 17:46:26 +0300

    feat:
    10 minutes

--- modules/networker/src/main/java/ru/vachok/networker/componentsrepo/server/TelnetServer.java
+++ modules/networker/src/main/java/ru/vachok/networker/componentsrepo/server/TelnetServer.java
@@ -4,7 +4,9 @@ package ru.vachok.networker.componentsrepo.server;
 
 
 import org.jetbrains.annotations.NotNull;
-import ru.vachok.networker.*;
+import ru.vachok.networker.AppComponents;
+import ru.vachok.networker.ExitApp;
+import ru.vachok.networker.SSHFactory;
 import ru.vachok.networker.data.enums.ConstantsFor;
 import ru.vachok.networker.data.enums.SwitchesWiFi;
 import ru.vachok.networker.net.ssh.Tracerouting;
@@ -20,39 +22,42 @@ import java.util.concurrent.TimeUnit;
 /**
  @see ru.vachok.networker.componentsrepo.server.TelnetServerTest
  @since 10.05.2019 (13:48) */
+@SuppressWarnings({"resource", "IOResourceOpenedButNotSafelyClosed"})
 public class TelnetServer implements ConnectToMe {
-    
-    
+
+
     private ServerSocket serverSocket;
-    
+
     private static final String JAR = "file:///G:/My_Proj/FtpClientPlus/modules/networker/ostpst/build/libs/";
-    
+
     private PrintStream printStreamF;
-    
+
     private Socket socket;
-    
+
     private int listenPort;
-    
+
     private static final MessageToUser messageToUser = MessageToUser.getInstance(MessageToUser.LOCAL_CONSOLE, TelnetServer.class.getSimpleName());
-    
+
+    private static final MessageToUser trayMsg = MessageToUser.getInstance(MessageToUser.TRAY, TelnetServer.class.getSimpleName());
+
     public static final String PR_LPORT = String.valueOf(9990);
-    
+
     public TelnetServer(int listenPort) {
         this.listenPort = listenPort;
         try {
             this.serverSocket = new ServerSocket(listenPort);
         }
         catch (IOException e) {
-            messageToUser.error("TelnetServer", "TelnetServer", e.getMessage() + " see line: 48");
+            messageToUser.error(getClass().getSimpleName(), "Constructor", e.getMessage() + " see line: 47");
         }
-        
+
     }
-    
+
     @Override public Socket getSocket() {
         runSocket();
         return this.socket;
     }
-    
+
     @Override public void runSocket() {
         Thread.currentThread().setName(this.getClass().getSimpleName());
         try {
@@ -61,12 +66,12 @@ public class TelnetServer implements ConnectToMe {
                 accepSoc();
             } while (!socket.isClosed());
         }
-        catch (Exception e) {
-            messageToUser.error("TelnetServer", "runSocket", e.getMessage() + " see line: 66");
+        catch (IOException e) {
+            messageToUser.error(getClass().getSimpleName(), "runSocket", e.getMessage() + " see line: 66");
             runSocket();
         }
     }
-    
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("TelnetServer{");
@@ -81,7 +86,7 @@ public class TelnetServer implements ConnectToMe {
         sb.append('}');
         return sb.toString();
     }
-    
+
     private void accepSoc() {
         int timeout = 0;
         try {
@@ -90,9 +95,9 @@ public class TelnetServer implements ConnectToMe {
             socket.setSoTimeout(timeout);
         }
         catch (SocketException e) {
-            messageToUser.error("TelnetServer", "accepSoc", e.getMessage() + " see line: 94");
+            messageToUser.error(getClass().getSimpleName(), "accepSoc", e.getMessage() + " see line: 94");
         }
-        
+
         try {
             InputStream iStream = socket.getInputStream();
             Scanner scanner = new Scanner(iStream);
@@ -106,25 +111,31 @@ public class TelnetServer implements ConnectToMe {
                 System.setIn(socket.getInputStream());
                 System.setOut(printStreamF);
                 if (socket.isConnected()) {
+                    trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
                     scanInput(scanner.nextLine());
                     printStream.print(iStream.read());
                 }
                 else {
                     System.setOut(System.err);
+                    trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
                     scanner.close();
                 }
             }
         }
         catch (IOException e) {
-            messageToUser.error("TelnetServer", "accepSoc", e.getMessage() + " see line: 120");
+            messageToUser.error(getClass().getSimpleName(), "accepSoc", e.getMessage() + " see line: 120");
             System.setOut(System.err);
+            trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
             reconSock();
         }
-        System.out.println(socket.isClosed() + " socket");
-        System.setOut(System.err);
-        reconSock();
+        finally {
+            System.out.println(socket.isClosed() + " socket");
+            System.setOut(System.err);
+            trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
+            reconSock();
+        }
     }
-    
+
     private void scanInput(@NotNull String scannerLine) throws IOException {
         if (scannerLine.contains("test")) {
             printStreamF.println("test OK");
@@ -132,22 +143,26 @@ public class TelnetServer implements ConnectToMe {
         }
         else if (scannerLine.equals("q")) {
             System.setOut(System.err);
+            trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
             accepSoc();
         }
         else if (scannerLine.equals(ConstantsFor.FILESUF_SSHACTIONS)) {
             try {
                 System.setOut(System.err);
+                trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
                 printStreamF.println(new Tracerouting().call());
                 accepSoc();
             }
             catch (Exception e) {
                 System.setOut(System.err);
+                trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
                 messageToUser.error("TelnetServer", "scanInput", e.getMessage() + " see line: 146");
                 socket.close();
             }
         }
         else if (scannerLine.contains("sshactions:")) {
             System.setOut(System.err);
+            trayMsg.info(getClass().getSimpleName(), socket.getLocalSocketAddress().toString(), socket.getRemoteSocketAddress().toString());
             String sshCom = scannerLine.split(":")[1];
             SSHFactory buildSSH = new SSHFactory.Builder(SwitchesWiFi.IPADDR_SRVGIT, sshCom, getClass().getSimpleName()).build();
             printStreamF.println(getClass().getSimpleName() + ".scanInput buildSSH  = " + buildSSH.call());
@@ -157,7 +172,7 @@ public class TelnetServer implements ConnectToMe {
             scanMore(scannerLine);
         }
     }
-    
+
     private void scanMore(@NotNull String line) throws IOException {
         if (line.equals("ost")) {
             String fileName = "\\\\192.168.14.10\\IT-Backup\\Mailboxes_users\\a.a.zavadskaya.pst";
@@ -180,7 +195,7 @@ public class TelnetServer implements ConnectToMe {
             System.setOut(System.err);
         }
     }
-    
+
     private @NotNull String loadLib() throws IOException {
         File ostJar = new File("ost.jar");
         StringBuilder stringBuilder = new StringBuilder();
@@ -207,7 +222,7 @@ public class TelnetServer implements ConnectToMe {
         ostJar.deleteOnExit();
         return stringBuilder.toString();
     }
-    
+
     private void reconSock() {
         this.socket = null;
         try {
@@ -215,7 +230,7 @@ public class TelnetServer implements ConnectToMe {
             accepSoc();
         }
         catch (IOException e) {
-            messageToUser.error("TelnetServer", "reconSock", e.getMessage() + " see line: 219");
+            messageToUser.error(getClass().getSimpleName(), "reconSock", e.getMessage() + " see line: 219");
         }
     }
 }
