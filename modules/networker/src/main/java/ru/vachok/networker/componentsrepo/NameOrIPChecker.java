57ac0582c39653c7b011dbf160e3fb70e4b76e88
Vachok <143500@gmail.com>
2020-03-23 14:53:32 +0300

    fix:
    16 seconds

--- modules/networker/src/main/java/ru/vachok/networker/componentsrepo/NameOrIPChecker.java
+++ modules/networker/src/main/java/ru/vachok/networker/componentsrepo/NameOrIPChecker.java
@@ -19,42 +19,27 @@ import java.util.regex.Pattern;
  @see NameOrIPCheckerTest
  @since 03.12.2018 (16:42) */
 public class NameOrIPChecker {
-    
-    
+
+
     /**
      {@link Pattern} локального имени в домене {@link ConstantsFor#DOMAIN_EATMEATRU}
      */
     private static final Pattern PATTERN_NAME = Pattern.compile("^(([apAdDTtNn])(([0-3])|([dDoOTtPp])){1,3})((\\d{2})|(\\d{4}))");
-    
+
     /**
      {@link MessageLocal}
      */
     private static final MessageToUser messageToUser = MessageToUser.getInstance(MessageToUser.LOCAL_CONSOLE, NameOrIPChecker.class.getSimpleName());
-    
+
     /**
      Ввод от юзера
      */
     private String nameOrIpStr;
-    
+
     public void setNameOrIpStr(String nameOrIpStr) {
         this.nameOrIpStr = nameOrIpStr;
     }
-    
-    /**
-     Default Constructor
-     <p>
- 
-     @param nameOrIpStr ввод из <a href="http://rups00.eatmeat.ru:8880/sshacts">SSHACTS</a>.
-     */
-    public NameOrIPChecker(@NotNull String nameOrIpStr) {
-        if (nameOrIpStr.contains(ConstantsFor.STR_EATMEAT)) {
-            this.nameOrIpStr = nameOrIpStr.split(ConstantsFor.STR_EATMEAT)[0];
-        }
-        else {
-            this.nameOrIpStr = nameOrIpStr;
-        }
-    }
-    
+
     public boolean isLocalAddress() {
         boolean result = false;
         try {
@@ -63,20 +48,20 @@ public class NameOrIPChecker {
                 result = true;
             }
         }
-        catch (UnknownFormatConversionException e) {
-            messageToUser.info(NameOrIPChecker.class.getSimpleName(), "isLocalAddress", e.getMessage() + Thread.currentThread().getState().name());
+        catch (UnknownFormatConversionException ignore) {
+            // 23.03.2020 (14:53)
         }
         return result;
     }
-    
+
     /**
      Преобразование пользовательского ввода в {@link InetAddress}
      <p>
      Проверяет по: {@link ConstantsFor#PATTERN_IP} и {@link #PATTERN_NAME}, что именно прилетело из броузера. <br>
      В зависимости от р-та проверки, резолвит и отдаёт {@link InetAddress}.
-     
+
      @return {@link InetAddress}
-     
+
      @throws UnknownFormatConversionException если не удалось опознать строку-ввод.
      */
     public InetAddress resolveInetAddress() {
@@ -94,7 +79,18 @@ public class NameOrIPChecker {
         }
         return inetAddress;
     }
-    
+
+    private InetAddress nameMach() {
+        String hostForResolve = nameOrIpStr + ConstantsFor.DOMAIN_EATMEATRU;
+        try {
+
+            return InetAddress.getByName(hostForResolve);
+        }
+        catch (UnknownHostException e) {
+            throw new UnknownFormatConversionException("Name not mach or no DNS record: " + hostForResolve);
+        }
+    }
+
     private InetAddress returnLocalIP() {
         InetAddress retAddress = InetAddress.getLoopbackAddress();
         try {
@@ -110,19 +106,9 @@ public class NameOrIPChecker {
         }
         return retAddress;
     }
-    
-    private InetAddress nameMach() {
-        String hostForResolve = nameOrIpStr + ConstantsFor.DOMAIN_EATMEATRU;
-        try {
-    
-            return InetAddress.getByName(hostForResolve);
-        }
-        catch (UnknownHostException e) {
-            throw new UnknownFormatConversionException("Name not mach or no DNS record: " + hostForResolve);
-        }
-    }
-    
-    private @NotNull InetAddress ipMach() {
+
+    @NotNull
+    private InetAddress ipMach() {
         byte[] addressBytes;
         try {
             addressBytes = InetAddress.getByName(nameOrIpStr).getAddress();
@@ -131,9 +117,24 @@ public class NameOrIPChecker {
         catch (UnknownHostException e) {
             throw new UnknownFormatConversionException("Ip is no local ip. " + nameOrIpStr);
         }
-        
+
     }
-    
+
+    /**
+     Default Constructor
+     <p>
+
+     @param nameOrIpStr ввод из <a href="http://rups00.eatmeat.ru:8880/sshacts">SSHACTS</a>.
+     */
+    public NameOrIPChecker(@NotNull String nameOrIpStr) {
+        if (nameOrIpStr.contains(ConstantsFor.STR_EATMEAT)) {
+            this.nameOrIpStr = nameOrIpStr.split(ConstantsFor.STR_EATMEAT)[0];
+        }
+        else {
+            this.nameOrIpStr = nameOrIpStr;
+        }
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("NameOrIPChecker{");
@@ -141,15 +142,16 @@ public class NameOrIPChecker {
         sb.append('}');
         return sb.toString();
     }
-    
+
     /**
      Резолвинг имени компа, или допись {@link ConstantsFor#DOMAIN_EATMEATRU}
      <p>
      {@link #resolveName()}
-     
+
      @return имя ПК
      */
-    private @NotNull String checkPat() {
+    @NotNull
+    private String checkPat() {
         StringBuilder stringBuilder = new StringBuilder();
         Matcher mName = PATTERN_NAME.matcher(nameOrIpStr);
         Matcher mIP = ConstantsFor.PATTERN_IP.matcher(nameOrIpStr);
@@ -168,7 +170,7 @@ public class NameOrIPChecker {
         }
         return stringBuilder.toString();
     }
-    
+
     private String resolveName() throws UnknownHostException {
         byte[] addressBytes = InetAddress.getByName(nameOrIpStr).getAddress();
         InetAddress inetAddress = InetAddress.getByAddress(addressBytes);
